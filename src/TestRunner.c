90a52409ad99fcf8a4d0d14cfc7da0e6de964a88
BenBornstein <bbornstein@sourceforge.net>
2003-04-07 23:56:07 +0000

    Added create_suite_SBMLWriter()

--- src/TestRunner.c
+++ src/TestRunner.c
@@ -80,6 +80,7 @@ Suite *create_suite_SBase                    (void);
 Suite *create_suite_SBMLDocument             (void);
 Suite *create_suite_SBMLFormatter            (void);
 Suite *create_suite_SBMLReader               (void);
+Suite *create_suite_SBMLWriter               (void);
 Suite *create_suite_Species                  (void);
 Suite *create_suite_SpeciesConcentrationRule (void);
 Suite *create_suite_SpeciesReference         (void);
@@ -163,6 +164,7 @@ main (void)
   srunner_add_suite( runner, create_suite_SBMLDocument             () );
   srunner_add_suite( runner, create_suite_SBMLFormatter            () );
   srunner_add_suite( runner, create_suite_SBMLReader               () );
+  srunner_add_suite( runner, create_suite_SBMLWriter               () );
   srunner_add_suite( runner, create_suite_Species                  () );
   srunner_add_suite( runner, create_suite_SpeciesConcentrationRule () );
   srunner_add_suite( runner, create_suite_SpeciesReference         () );
