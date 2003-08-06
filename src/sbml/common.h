2c8a3057d764c81c5f2d5f3c7d3043da2eca12f6
BenBornstein <bbornstein@sourceforge.net>
2003-08-06 23:29:00 +0000

    Factored-out LIBSBML_EXTERN and BEGIN/END_C_DECLS to extern.h.

--- src/sbml/common.h
+++ src/sbml/common.h
@@ -50,8 +50,8 @@
  */
 
 
-#ifndef SBML_COMMON_H
-#define SBML_COMMON_H 1
+#ifndef LIBSBML_COMMON_H
+#define LIBSBML_COMMON_H 1
 
 
 #if HAVE_CONFIG_H || WIN32
@@ -86,9 +86,7 @@
 #endif
 
 
-#if WIN32
-
-#if !defined(CYGWIN)
+#if WIN32 && !defined(CYGWIN)
 
 #undef  HAVE_LIBCHECK
 
@@ -103,43 +101,12 @@
  */
 #pragma warning(disable: 4800)
 
-#endif /* !CYGWIN */
-
-/**
- * The following ifdef block is the standard way of creating macros which
- * make exporting from a DLL simpler. All files within this DLL are
- * compiled with the LIBSBML_EXPORTS symbol defined on the command line.
- * This symbol should not be defined on any project that uses this
- * DLL. This way any other project whose source files include this file see
- * LIBSBML_EXTERN functions as being imported from a DLL, wheras this DLL
- * sees symbols defined with this macro as being exported.
- *
- * (From Andrew Finney's sbwdefs.h, with "SBW" replaced by "LIBSBML" :)
- */
-#if LIBSBML_EXPORTS
-#  define LIBSBML_EXTERN __declspec(dllexport)
-#else
-#  define LIBSBML_EXTERN __declspec(dllimport)
-#endif
-
-#else
-
-#define LIBSBML_EXTERN
-
-#endif  /* WIN32 */
+#endif /* WIN32 && !CYGWIN */
 
 
+#include "extern.h"
 #include "memory.h"
 #include "util.h"
 
 
-#if __cplusplus
-#  define BEGIN_C_DECLS extern "C" {
-#  define END_C_DECLS   }
-#else
-#  define BEGIN_C_DECLS
-#  define END_C_DECLS
-#endif
-
-
-#endif  /** SBML_COMMON_H **/
+#endif  /** LIBSBML_COMMON_H **/
