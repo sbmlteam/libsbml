b75e214641c11608e8728a1b9f09a00e06e104e9
shoops <shoops@copasi.org>
2005-04-29 16:52:23 +0000

    Added C declarations since util is containing C code.

--- examples/util.h
+++ examples/util.h
@@ -48,6 +48,7 @@
  * Contributor(s):
  */
 
+BEGIN_C_DECLS
 
 /**
  * @return the number of milliseconds elapsed since the Epoch.
@@ -60,3 +61,5 @@ getCurrentMillis (void);
  */
 unsigned long
 getFileSize (const char *filename);
+
+END_C_DECLS
