312dfeb87502c69c9d96377d4725798a1af62295
skeating <s.keating@ucl.ac.uk>
2005-08-08 17:27:15 +0000

    simple dos batch file that runs through the example programs built with windows and checks that each performs correctly
    ****NOTE: very basic checking; but establishes that the windows build has been successful

---
+++ win32/test/testWindowsBuild.bat
@@ -0,0 +1,24 @@
+@echo off
+cd ..
+cd bin
+readSBML ../test/test.xml
+if ERRORLEVEL 0 echo readSBML passed
+pause
+validateSBML ../test/test.xml
+if ERRORLEVEL 0 echo validateSBML passed
+pause
+printSBML ../test/test.xml
+if ERRORLEVEL 0 echo printSBML passed
+pause
+printMath ../test/test.xml
+if ERRORLEVEL 0 echo printMath passed
+pause
+convertSBML ../test/test.xml ../test/test-new.xml
+if ERRORLEVEL 0 validateSBML ../test/test-new.xml
+if ERRORLEVEL 0 echo conversion produced a valid level 2 file
+pause
+convertSBML ../test/test-new.xml ../test/test-new1.xml
+if ERRORLEVEL 0 validateSBML ../test/test-new1.xml
+if ERRORLEVEL 0 echo conversion produced a valid level 1 file
+cd ..
+cd test
