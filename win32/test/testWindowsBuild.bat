8e1423581ae477229c3e56ae92f38ef1da6e0594
skeating <s.keating@ucl.ac.uk>
2005-09-10 11:14:22 +0000

    removed msvc6 files

--- win32/test/testWindowsBuild.bat
+++ win32/test/testWindowsBuild.bat
@@ -20,5 +20,8 @@ pause
 convertSBML ../test/test-new.xml ../test/test-new1.xml
 if ERRORLEVEL 0 validateSBML ../test/test-new1.xml
 if ERRORLEVEL 0 echo conversion produced a valid level 1 file
+drawMath ../test/test.xml
+if ERRORLEVEL 0 echo drawMath passed
+pause
 cd ..
 cd test
