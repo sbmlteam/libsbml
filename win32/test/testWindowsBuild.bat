989038b40fe31bf3659064d2ad06cd69856fe0cd
skeating <s.keating@ucl.ac.uk>
2005-11-01 10:40:56 +0000

    corrected call to drawMath

--- win32/test/testWindowsBuild.bat
+++ win32/test/testWindowsBuild.bat
@@ -20,7 +20,7 @@ pause
 convertSBML ../test/test-new.xml ../test/test-new1.xml
 if ERRORLEVEL 0 validateSBML ../test/test-new1.xml
 if ERRORLEVEL 0 echo conversion produced a valid level 1 file
-drawMath ../test/test.xml
+drawMath ../test/test.xml ../test/drawout.dot
 if ERRORLEVEL 0 echo drawMath passed
 pause
 cd ..
