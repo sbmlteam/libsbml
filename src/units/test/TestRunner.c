755eac891957c4660dfdd36b05b6a41e69e5f940
BenBornstein <bbornstein@sourceforge.net>
2006-02-06 22:47:33 +0000

    With strict compiler settings, C++-style comments in a C file cause an error.

--- src/units/test/TestRunner.c
+++ src/units/test/TestRunner.c
@@ -79,7 +79,7 @@ main (void)
   SRunner *runner = srunner_create( create_suite_UtilsUnit() );
 
 
-  //srunner_add_suite( runner, create_suite_FormulaFormatter () );
+  /* srunner_add_suite( runner, create_suite_FormulaFormatter () ); */
   
 
 #ifdef TRACE_MEMORY
