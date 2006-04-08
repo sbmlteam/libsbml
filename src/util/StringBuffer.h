aef71716711345a5ccb4df7c38c3a4b5bd5ff4e4
BenBornstein <bbornstein@sourceforge.net>
2006-04-08 07:49:18 +0000

    Changed header file #include locations and pre-Hackathon updates that would have been checked-in a week ago if not for the SF.net CVS outage.

--- src/util/StringBuffer.h
+++ src/util/StringBuffer.h
@@ -2,56 +2,30 @@
  * \file        StringBuffer.h
  * \brief       A growable buffer for creating character strings.
  * \author      Ben Bornstein <ben.bornstein@jpl.nasa.gov>
- * \version     $Id StringBuffer.h,v 1.5 2004/04/16 013708 mhucka Exp $
- * \version     $Source /cvsroot/sbml/libsbml/src/sbml/StringBuffer.h,v $
+ *
+ * $Id$
+ * $Source$
  */
-/* Copyright 2003-2004 California Institute of Technology and
- * Japan Science and Technology Corporation.
+/* Copyright 2002 California Institute of Technology and Japan Science and
+ * Technology Corporation.
  *
  * This library is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published
- * by the Free Software Foundation; either version 2.1 of the License, or
- * any later version.
- *
- * This library is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF
- * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.  The software and
- * documentation provided hereunder is on an "as is" basis, and the
- * California Institute of Technology and Japan Science and Technology
- * Corporation have no obligations to provide maintenance, support,
- * updates, enhancements or modifications.  In no event shall the
- * California Institute of Technology or the Japan Science and Technology
- * Corporation be liable to any party for direct, indirect, special,
- * incidental or consequential damages, including lost profits, arising
- * out of the use of this software and its documentation, even if the
- * California Institute of Technology and/or Japan Science and Technology
- * Corporation have been advised of the possibility of such damage.  See
- * the GNU Lesser General Public License for more details.
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation.  A copy of the license agreement is
+ * provided in the file named "LICENSE.txt" included with this software
+ * distribution.  It is also available online at
+ * http://sbml.org/software/libsbml/license.html
  *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this library; if not, write to the Free Software Foundation,
  * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
- *
- * The original code contained here was initially developed by:
- *
- *     Ben Bornstein
- *     The SBML Team
- *     Control and Dynamical Systems, MC 107-81
- *     California Institute of Technology
- *     Pasadena, CA, 91125, USA
- *
- *     http://www.sbml.org
- *     mailto:sbml-team@caltech.edu
- *
- * Contributor(s):
- *
  */
 
 #ifndef StringBuffer_h
 #define StringBuffer_h
 
 
-#include "../common/extern.h"
+#include <sbml/common/extern.h>
 
 
 BEGIN_C_DECLS
