17d755741c57bb5438ef7a0fdc0c54e4964426e2
mhucka <mhucka@caltech.edu>
2007-04-20 22:42:44 +0000

    Added explanation of LIBSBML_FLOAT_FORMAT.

--- src/common/common.h
+++ src/common/common.h
@@ -1,26 +1,25 @@
 /**
  * @file    common.h
- * @brief   Common functions and macros for C and C++
+ * @brief   Common functions and macros for C and C++.
  * @author  Ben Bornstein
  *
  * $Id$
  * $Source$
- */
-/* Copyright 2003 California Institute of Technology and Japan Science and
- * Technology Corporation.
  *
+ *<!---------------------------------------------------------------------------
+ * This file is part of libSBML.  Please visit http://sbml.org for more
+ * information about SBML, and the latest version of libSBML.
+ *
+ * Copyright 2005-2007 California Institute of Technology.
+ * Copyright 2002-2005 California Institute of Technology and
+ *                     Japan Science and Technology Corporation.
+ * 
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation.  A copy of the license agreement is
- * provided in the file named "LICENSE.txt" included with this software
- * distribution.  It is also available online at
- * http://sbml.org/software/libsbml/license.html
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this library; if not, write to the Free Software Foundation,
- * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
- */
-
+ * the Free Software Foundation.  A copy of the license agreement is provided
+ * in the file named "LICENSE.txt" included with this software distribution and
+ * also available online as http://sbml.org/software/libsbml/license.html
+ *----------------------------------------------------------------------- -->*/
 
 #ifndef LIBSBML_COMMON_H
 #define LIBSBML_COMMON_H 1
@@ -53,6 +52,11 @@
 #endif
 
 
+/**
+ * Default format used by libSBML when writing out floating-point numbers
+ * into the XML produced by libSBML.  This is used by
+ * StringBuffer_appendReal.
+ */
 #define LIBSBML_FLOAT_FORMAT "%.15g"
 
 
