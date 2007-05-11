2b41dd3d9845c1fcd787350f6b375697674ca132
mhucka <mhucka@caltech.edu>
2007-05-11 04:01:57 +0000

    Updated file header.

--- examples/c++/translateMath.cpp
+++ examples/c++/translateMath.cpp
@@ -1,24 +1,14 @@
 /**
- * \file    translateMath.cpp
- * \brief   Translates infix formulas into MathML and vice-versa
- * \author  Sarah Keating and Ben Bornstein
+ * @file    translateMath.cpp
+ * @brief   Translates infix formulas into MathML and vice-versa
+ * @author  Sarah Keating
+ * @author  Ben Bornstein
  *
  * $Id$
  * $Source$
- */
-/* Copyright 2003 California Institute of Technology and Japan Science and
- * Technology Corporation.
  *
- * This library is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation.  A copy of the license agreement is
- * provided in the file named "LICENSE.txt" included with this software
- * distribution.  It is also available online at
- * http://sbml.org/software/libsbml/license.html
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this library; if not, write to the Free Software Foundation,
- * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+ * This file is part of libSBML.  Please visit http://sbml.org for more
+ * information about SBML, and the latest version of libSBML.
  */
 
 
