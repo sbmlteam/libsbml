91db6d4611c649c47087aca1217469e482b0a3a8
mhucka <mhucka@caltech.edu>
2007-05-11 04:01:30 +0000

    Minor update and cleanup for libSBML 3.0.0.

--- examples/c++/printSBML.cpp
+++ examples/c++/printSBML.cpp
@@ -1,24 +1,15 @@
 /**
- * \file    printModel.cpp
- * \brief   Prints some information about the top-level model
- * \author  Sarah Keating and Ben Bornstein
+ * @file    printModel.cpp
+ * @brief   Prints some information about the top-level model
+ * @author  Sarah Keating
+ * @author  Ben Bornstein
+ * @author  Michael Hucka
  *
  * $Id$
  * $Source$
- */
-/* Copyright 2003 California Institute of Technology and Japan Science and
- * Technology Corporation.
  *
- * This library is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation.  A copy of the license agreement is
- * provided in the file named "LICENSE.txt" included with this software
- * distribution.  It is also available online at
- * http://sbml.org/software/libsbml/license.html
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this library; if not, write to the Free Software Foundation,
- * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+ * This file is part of libSBML.  Please visit http://sbml.org for more
+ * information about SBML, and the latest version of libSBML.
  */
 
 
@@ -32,62 +23,58 @@ using namespace std;
 int
 main (int argc, char *argv[])
 {
-  const char* filename;
-
-  SBMLDocument* d;
-  Model*        m;
-
-  unsigned int level, version;
-
-
   if (argc != 2)
   {
-    cout << endl << "  usage: printSBML <filename>" << endl << endl;
-    return 2;
+    cout << endl << "Usage: printSBML <filename>" << endl << endl;
+    return 1;
   }
 
+  const char* filename   = argv[1];
+  SBMLDocument* document = readSBML(filename);
 
-  filename = argv[1];
-  d        = readSBML(filename);
+  if (document->getNumErrors() > 0)
+  {
+    cerr << "Encountered the following SBML errors:" << endl;
+    document->printErrors(cerr);
+    return 1;
+  }
 
-  d->printErrors(cout);
-
-  m = d->getModel();
-
-  level   = d->getLevel  ();
-  version = d->getVersion();
+  unsigned int level   = document->getLevel  ();
+  unsigned int version = document->getVersion();
 
   cout << endl
        << "File: " << filename
        << " (Level " << level << ", version " << version << ")" << endl;
 
-  cout << "         ";
+  Model *model = document->getModel();
 
-  if (m == 0)
+  if (model == NULL)
   {
     cout << "No model present." << endl;
     return 1;
   }
 
-  cout << "         ";
-  cout << "  model id: " <<  (m->isSetId() ? m->getId() : "(empty)") << endl;
+  cout << "               "
+       << (level == 1 ? "name: " : "  id: ")
+       << (model->isSetId() ? model->getId() : "(empty)") << endl;
 
-  if (m->isSetSBOTerm())
-    cout << "sboTerm: " << m->getSBOTerm() << endl;
-  cout << "functionDefinitions: " << m->getNumFunctionDefinitions() << endl;
-  cout << "    unitDefinitions: " << m->getNumUnitDefinitions    () << endl;
-  cout << "   compartmentTypes: " << m->getNumCompartmentTypes   () << endl;
-  cout << "        specieTypes: " << m->getNumSpeciesTypes       () << endl;
-  cout << "       compartments: " << m->getNumCompartments       () << endl;
-  cout << "            species: " << m->getNumSpecies            () << endl;
-  cout << "         parameters: " << m->getNumParameters         () << endl;
-  cout << " initialAssignments: " << m->getNumInitialAssignments () << endl;
-  cout << "              rules: " << m->getNumRules              () << endl;
-  cout << "        constraints: " << m->getNumConstraints        () << endl;
-  cout << "          reactions: " << m->getNumReactions          () << endl;
-  cout << "             events: " << m->getNumEvents             () << endl;
+  if (model->isSetSBOTerm())
+    cout << "      model sboTerm: " << model->getSBOTerm() << endl;
+
+  cout << "functionDefinitions: " << model->getNumFunctionDefinitions() << endl;
+  cout << "    unitDefinitions: " << model->getNumUnitDefinitions    () << endl;
+  cout << "   compartmentTypes: " << model->getNumCompartmentTypes   () << endl;
+  cout << "        specieTypes: " << model->getNumSpeciesTypes       () << endl;
+  cout << "       compartments: " << model->getNumCompartments       () << endl;
+  cout << "            species: " << model->getNumSpecies            () << endl;
+  cout << "         parameters: " << model->getNumParameters         () << endl;
+  cout << " initialAssignments: " << model->getNumInitialAssignments () << endl;
+  cout << "              rules: " << model->getNumRules              () << endl;
+  cout << "        constraints: " << model->getNumConstraints        () << endl;
+  cout << "          reactions: " << model->getNumReactions          () << endl;
+  cout << "             events: " << model->getNumEvents             () << endl;
   cout << endl;
 
-  delete d;
+  delete document;
   return 0;
 }
--- examples/c++/readSBML.cpp
+++ examples/c++/readSBML.cpp
@@ -1,24 +1,14 @@
 /**
- * \file    readSBML.cpp
- * \brief   Similar to validateSBML, but without the validation
- * \author  Sarah Keating and Ben Bornstein
+ * @file    readSBML.cpp
+ * @brief   Similar to validateSBML, but without the validation
+ * @author  Sarah Keating
+ * @author  Ben Bornstein
  *
  * $Id$
  * $Source$
- */
-/* Copyright 2003 California Institute of Technology and Japan Science and
- * Technology Corporation.
  *
- * This library is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation.  A copy of the license agreement is
- * provided in the file named "LICENSE.txt" included with this software
- * distribution.  It is also available online at
- * http://sbml.org/software/libsbml/license.html
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this library; if not, write to the Free Software Foundation,
- * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+ * This file is part of libSBML.  Please visit http://sbml.org for more
+ * information about SBML, and the latest version of libSBML.
  */
 
 
@@ -34,38 +24,32 @@ using namespace std;
 int
 main (int argc, char *argv[])
 {
-  const char* filename;
-
-  SBMLDocument* d;
-
-  unsigned long start, stop, size;
-  unsigned int  errors;
-
-
   if (argc != 2)
   {
-    cout << endl << "  usage: readSBML <filename>" << endl << endl;
-    return 2;
+    cout << endl << "Usage: readSBML <filename>" << endl << endl;
+    return 1;
   }
 
-  filename = argv[1];
+  const char* filename   = argv[1];
+  SBMLDocument* document;
+  SBMLReader reader;
+  unsigned long start, stop;
 
-  start = getCurrentMillis();
-  d     = readSBML(filename);
-  stop  = getCurrentMillis();
+  start    = getCurrentMillis();
+  document = reader.readSBML(filename);
+  stop     = getCurrentMillis();
 
-  errors = d->getNumErrors();
-  size   = getFileSize(argv[1]);
+  unsigned int errors = document->getNumErrors();
 
   cout << endl;
-  cout << "        filename: " << filename     << endl;
-  cout << "       file size: " << size         << endl;
-  cout << "  read time (ms): " << stop - start << endl;
-  cout << "        error(s): " << errors       << endl;
-
-  if (errors > 0) d->printErrors(cout);
+  cout << "            filename: " << filename              << endl;
+  cout << "           file size: " << getFileSize(filename) << endl;
+  cout << "      read time (ms): " << stop - start          << endl;
+  cout << " validation error(s): " << errors << endl;
   cout << endl;
 
-  delete d;
+  document->printErrors(cerr);
+
+  delete document;
   return errors;
 }
