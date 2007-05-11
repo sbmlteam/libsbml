a2a7d6f7becacd344b2e1658dc8cd5f16b3aaae0
mhucka <mhucka@caltech.edu>
2007-05-11 05:03:12 +0000

    Minor update and cleanup for libSBML 3.0.0.

--- examples/c++/validateSBML.cpp
+++ examples/c++/validateSBML.cpp
@@ -1,24 +1,14 @@
 /**
- * \file    validateSBML.cpp
- * \brief   Validates an SBML file against the appropriate schema
- * \author  Sarah Keating and Ben Bornstein
+ * @file    validateSBML.cpp
+ * @brief   Validates an SBML file against the appropriate schema
+ * @author  Sarah Keating
+ * @author  Ben Bornstein
  *
  * $Id$
  * $Source$
- */
-/* Copyright 2003 California Institute of Technology and Japan Science and
- * Technology Corporation.
  *
- * This library is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation.  A copy of the license agreement is
- * provided in the file named "LICENSE.txt" included with this software
- * distribution.  It is also available online at
- * http://sbml.org/software/libsbml/license.html
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this library; if not, write to the Free Software Foundation,
- * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+ * This file is part of libSBML.  Please visit http://sbml.org for more
+ * information about SBML, and the latest version of libSBML.
  */
 
 
@@ -34,44 +24,50 @@ using namespace std;
 int
 main (int argc, char *argv[])
 {
-  const char* filename;
-
-  unsigned long start, stop, size;
-  unsigned int  errors = 0;
-
-  SBMLDocument *d;
-
-
   if (argc != 2)
   {
-    cout << endl << " usage: validateSBML <filename>" << endl << endl;
-    return 2;
+    cout << endl << "Usage: validateSBML <filename>" << endl << endl;
+    return 1;
   }
 
+  const char* filename = argv[1];
+  SBMLDocument *document;
+  SBMLReader reader;
+  unsigned long start, stop;
+  unsigned int errors;
 
+  start    = getCurrentMillis();
+  document = reader.readSBML(filename);
+  stop     = getCurrentMillis();
 
-  SBMLReader sr;
-
-  filename = argv[1];
-
-  start = getCurrentMillis();
-  d     = sr.readSBML(filename);
-  stop  = getCurrentMillis();
-
-  errors  = d->getNumErrors();
-  errors += d->checkConsistency();
-
-  size = getFileSize(filename);
+  errors = document->getNumErrors();
 
   cout << endl;
-  cout << "        filename: " << filename     << endl;
-  cout << "       file size: " << size         << endl;
-  cout << "  read time (ms): " << stop - start << endl;
-  cout << "        error(s): " << errors       << endl;
+  cout << "             filename: " << filename              << endl;
+  cout << "            file size: " << getFileSize(filename) << endl;
+  cout << "       read time (ms): " << stop - start          << endl;
 
-  if (errors > 0) d->printErrors(cout);
-  cout << endl;
+  if (errors > 0)
+  {
+    cerr << "Encountered the following SBML errors:" << endl;
+    document->printErrors(cerr);
+    cerr << endl << "Further consistency checking aborted." << endl;
+  }
+  else
+  {
+    errors = document->checkConsistency();
+    if (errors > 0)
+    {
+      cout << " consistency error(s): " << errors << endl;
+      cout << endl;
+      document->printErrors(cerr);
+    }
+    else
+    {
+      cout << "               errors: 0" << endl;
+    }
+  }
 
-  delete d;
+  delete document;
   return errors;
 }
