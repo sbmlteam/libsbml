419004f89e911ff5b2fc995fb14c49a62ed2f175
mhucka <mhucka@caltech.edu>
2007-05-12 02:11:56 +0000

    Completely rewrote this to implement different logic and use a simple rule for which direction the conversion should be performed in.  The rule is: if it's not L2v3 already, convert to L2v3, else convert to L1v2.

--- examples/c++/convertSBML.cpp
+++ examples/c++/convertSBML.cpp
@@ -1,139 +1,148 @@
 /**
- * \file    convertSBML.cpp
- * \brief   Converts SBML L1 documents (any version) to L2v1
- * \author  Sarah Keating and Ben Bornstein
+ * @file    convertSBML.cpp
+ * @brief   Converts SBML L1 documents (any version) to L2v1
+ * @author  Michael Hucka
+ * @author  Sarah Keating
+ * @author  Ben Bornstein
  *
  * $Id$
  * $Source$
- */
-/* Copyright 2003 California Institute of Technology and Japan Science and
- * Technology Corporation.
  *
- * This library is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation.  A copy of the license agreement is
- * provided in the file named "LICENSE.txt" included with this software
- * distribution.  It is also available online at
- * http://sbml.org/software/libsbml/license.html
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this library; if not, write to the Free Software Foundation,
- * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+ * This file is part of libSBML.  Please visit http://sbml.org for more
+ * information about SBML, and the latest version of libSBML.
  */
 
 
 #include <iostream>
 #include <sbml/SBMLTypes.h>
 
-
 using namespace std;
 
 
+#define LATEST_SBML_LEVEL   2
+#define LATEST_SBML_VERSION 3
+
+/*
+ * 'ignorable' is a list of libSBML error codes that can be ignored for the
+ * purposes of this conversion program.  Some conversions are not fatal,
+ * but indicate loss of information that does not affect the mathematical
+ * structure of a model.
+ */
+static unsigned int ignorable[] = {
+  92003,
+  92004,
+  92005,
+  92006,
+  93001,
+  91003,
+  91005,
+  91006
+};
+
+/*
+ * Predicate returning true if the errors encountered are not ignorable.
+ */
+bool
+conversion_errors(SBMLDocument* document, unsigned int errors)
+{
+  for (unsigned int i = 0; i < errors; i++)
+  {
+    bool failure = true;
+
+    for (unsigned int n = 0; i < sizeof(ignorable)/sizeof(ignorable[0]); n++)
+    {
+      if (document->getError(i)->getId() == ignorable[n])
+      {
+	failure = false;
+	break;
+      }
+    }
+
+    if (failure) return failure;
+  }
+
+  return false;
+}
+
+
+/*
+ * Main routine.
+ */
 int
 main (int argc, char *argv[])
 {
-  SBMLDocument* d;
-  unsigned int  errors;
-
-
   if (argc != 3)
   {
-    cout << endl
-         << "  usage: convertSBML <input-filename> <output-filename>" << endl
-         << "  Converts an SBML L1 file to L2 or vice versa"          << endl
-         << endl;
-    return 2;
+    cout << "Usage: convertSBML <input-filename> <output-filename>" << endl
+	 << "This program will attempt to convert a model either to" << endl
+	 << "SBML Level " << LATEST_SBML_LEVEL
+	 << " Version " << LATEST_SBML_VERSION
+	 << " (if the model is not already) or, if" << endl
+	 << "the model is already expressed in Level " << LATEST_SBML_LEVEL
+	 << " Version " << LATEST_SBML_VERSION << ", this" << endl
+	 << "program will attempt to convert the model to Level 1 Version 2."
+	 << endl;
+    return 1;
   }
 
+  const char* inputFile   = argv[1];
+  const char* outputFile  = argv[2];
 
-  d      = readSBML(argv[1]);
-  errors = d->getNumErrors();
+  SBMLDocument* document  = readSBML(inputFile);
+  unsigned int  errors    = document->getNumErrors();
 
   if (errors > 0)
   {
-    cout << "Read Error(s):" << endl;
-	  d->printErrors(cout);
+    cerr << "Encountered the following SBML errors:" << endl;
+    document->printErrors(cerr);
+    cerr << "Conversion skipped.  Please correct the problems above first."
+	 << endl;
+    return errors;
+  }
 
-    cout << "Conversion skipped.  Correct the above and re-run." << endl;
+  /**
+   * If the given model is not already L2v3, assume that the user wants to
+   * convert it to the latest release of SBML (which is L2v3 currently).
+   * If the model is already L2v3, assume that the user wants to attempt to
+   * convert it down to Level 1 (specifically L1v2).
+   */
+
+  unsigned int olevel   = document->getLevel();
+  unsigned int oversion = document->getVersion();
+
+  if (olevel < LATEST_SBML_LEVEL || oversion < LATEST_SBML_VERSION)
+  {
+    cout << "Attempting to convert Level " << olevel << " Version " << oversion
+	 << " model to Level " << LATEST_SBML_LEVEL
+	 << " Version " << LATEST_SBML_VERSION << "."  << endl;
+    document->setLevelAndVersion(LATEST_SBML_LEVEL, LATEST_SBML_VERSION);
   }
   else
   {
-    unsigned int orig_level = d->getLevel();
-    unsigned int orig_version = d->getVersion();
-
-    unsigned int level   = 2;
-    unsigned int version = 1;
-
-    d->setLevelAndVersion(level, version);
-  
-    errors = d->getNumErrors();
-
-    /**
-     * some conversions will report errors that merely lose information
-     * and do not effect the actual model - and so conversion is possible
-     * whilst in other cases elements have been used that cannot be translated
-     */
-
-    unsigned int fatal = 0;
-    unsigned int errorID;
-
-    if (errors > 0)
-    {
-      if (orig_level == 2 && level == 1)
-      {
-        fatal = 1;
-      }
-      else if (orig_level == 2 && level == 2)
-      {
-        if (orig_version == 3 && version == 2)
-        {
-          fatal = 2;
-        }
-        else if ((orig_version == 3 || orig_version == 2) && version == 1)
-        {
-          unsigned int n = 0;
-          while(fatal == 0 && n < d->getNumErrors())
-          {
-            errorID = d->getError(n)->getId();
-
-            if ( (errorID == 92001)
-              || (errorID == 92002))
-            {
-              fatal = 1;
-            }
-            else
-            {
-              fatal = 2;
-            }
-            n++;
-          }
-        }
-      }
-    }
-
-    if (fatal == 1)
-    {
-      cout << "Conversion Error(s):" << endl;
-      d->printErrors(cout);
-
-      cout << "Conversion skipped.  Either libSBML does not (yet) have " << endl
-          << "ability to convert this model or (automatic) conversion " << endl
-          << "is not possible." << endl;
-    }
-    else if (fatal == 2)
-    {
-      cout << "Loss of information Error(s):" << endl;
-      d->printErrors(cout);
-
-      writeSBML(d, argv[2]);
-    }
-    else
-    {
-      writeSBML(d, argv[2]);
-    }
+    cout << "Attempting to convert Level " << olevel << " Version " << oversion
+	 << " model to Level 1 Version 2." << endl;
+    document->setLevelAndVersion(1, 2);
   }
 
+  errors = document->getNumErrors();
 
-  delete d;
-  return errors;
+  if (conversion_errors(document, errors))
+  {
+    cerr << "Unable to perform conversion due to the following:" << endl;
+    document->printErrors(cerr);
+    cout << endl;
+    cout << "Conversion skipped.  Either libSBML does not (yet)" << endl
+	 << "have the ability to convert this model or (automatic)" << endl
+	 << "conversion is not possible in this case." << endl;
+
+    return errors;
+  }    
+  else
+  {
+    cout << "Conversion completed." << endl;
+    writeSBML(document, outputFile);
+  }
+
+  return 0;
 }
+
