c306128002cd94ae0620c6206d9cf8d270d54099
mhucka <mhucka@caltech.edu>
2007-06-03 07:37:37 +0000

    Mild reformatting to fit with style of other examples in libSBML 3.0.

--- examples/c/readSBML.c
+++ examples/c/readSBML.c
@@ -1,26 +1,14 @@
 /**
- * \file    readSBML.c
- * \brief   Similar to validateSBML, but without the validation
- * \author  Ben Bornstein
+ * @file    readSBML.c
+ * @brief   Similar to validateSBML, but without the validation
+ * @author  Ben Bornstein
  *
  * $Id$
  * $Source$
- */
-/* Copyright 2003 California Institute of Technology and Japan Science and
- * Technology Corporation.
  *
- * This library is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation.  A copy of the license agreement is
- * provided in the file named "LICENSE.txt" included with this software
- * distribution.  It is also available online at
- * http://sbml.org/software/libsbml/license.html
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this library; if not, write to the Free Software Foundation,
- * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
+ * This file is part of libSBML.  Please visit http://sbml.org for more
+ * information about SBML, and the latest version of libSBML.
  */
-
 
 #include <stdio.h>
 
@@ -41,7 +29,7 @@ main (int argc, char *argv[])
 
   if (argc != 2)
   {
-    printf("\n  usage: readSBML <filename>\n\n");
+    printf("Usage: readSBML filename\n");
     return 2;
   }
 
