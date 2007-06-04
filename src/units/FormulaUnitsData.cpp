37119eb4ee1cbacacfae03b991ce724f3ffc31a1
mhucka <mhucka@caltech.edu>
2007-06-04 17:32:10 +0000

    Added new file header & doxygen tags to flag this as internal code.

--- src/units/FormulaUnitsData.cpp
+++ src/units/FormulaUnitsData.cpp
@@ -1,26 +1,27 @@
 /**
+ * @cond doxygen-libsbml-internal
+ *
  * @file    FormulaUnitsData.cpp
  * @brief   Class for storing information relating to units of a formula
  * @author  SBML Team <sbml-team@caltech.edu>
  *
  * $Id$
  * $Source$
- */
-/* Copyright 2003 California Institute of Technology and Japan Science and
- * Technology Corporation.
  *
+ *<!---------------------------------------------------------------------------
+ * This file is part of libSBML.  Please visit http://sbml.org for more
+ * information about SBML, and the latest version of libSBML.
+ *
+ * Copyright 2005-2007 California Institute of Technology.
+ * Copyright 2002-2005 California Institute of Technology and
+ *                     Japan Science and Technology Corporation.
+ * 
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation.  A copy of the license agreement is
- * provided in the file named "LICENSE.txt" included with this software
- * distribution.  It is also available online at
- * http://sbml.org/software/libsbml/license.html
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this library; if not, write to the Free Software Foundation,
- * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
- */
-
+ * the Free Software Foundation.  A copy of the license agreement is provided
+ * in the file named "LICENSE.txt" included with this software distribution and
+ * also available online as http://sbml.org/software/libsbml/license.html
+ *----------------------------------------------------------------------- -->*/
 
 #include <sbml/Model.h>
 #include <sbml/units/FormulaUnitsData.h>
@@ -611,3 +612,6 @@ ListFormulaUnitsData::getTypeCode () const
   return SBML_LIST_FORMULA_UNITS_DATA;
 }
 
+
+
+/** @endcond doxygen-libsbml-internal */
--- src/units/FormulaUnitsData.h
+++ src/units/FormulaUnitsData.h
@@ -1,25 +1,27 @@
 /**
+ * @cond doxygen-libsbml-internal
+ *
  * @file    FormulaUnitsData.h
  * @brief   Class for storing information relating to units of a formula
  * @author  SBML Team <sbml-team@caltech.edu>
  *
  * $Id$
  * $Source$
- */
-/* Copyright 2003 California Institute of Technology and Japan Science and
- * Technology Corporation.
  *
+ *<!---------------------------------------------------------------------------
+ * This file is part of libSBML.  Please visit http://sbml.org for more
+ * information about SBML, and the latest version of libSBML.
+ *
+ * Copyright 2005-2007 California Institute of Technology.
+ * Copyright 2002-2005 California Institute of Technology and
+ *                     Japan Science and Technology Corporation.
+ * 
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation.  A copy of the license agreement is
- * provided in the file named "LICENSE.txt" included with this software
- * distribution.  It is also available online at
- * http://sbml.org/software/libsbml/license.html
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this library; if not, write to the Free Software Foundation,
- * Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
- */
+ * the Free Software Foundation.  A copy of the license agreement is provided
+ * in the file named "LICENSE.txt" included with this software distribution and
+ * also available online as http://sbml.org/software/libsbml/license.html
+ *----------------------------------------------------------------------- -->*/
 
 #ifndef FormulaUnitsData_h
 #define FormulaUnitsData_h
@@ -220,3 +222,6 @@ public:
 
 
 #endif
+
+
+/** @endcond doxygen-libsbml-internal */
