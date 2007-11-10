9c956647d84dd03f302843264860e009ac07b55c
mhucka <mhucka@caltech.edu>
2007-11-10 00:38:29 +0000

    Short script for running tests from within 'make check'.

---
+++ src/bindings/matlab/test/runTests.m
@@ -0,0 +1,10 @@
+%% Description       : Script for running MATLAB tests
+%% Original author(s): Michael Hucka <mhucka@caltech.edu>
+%% Organization      : California Institute of Technology
+%% $Id$
+%% $Source$
+
+addpath('..')
+addpath('../../..')
+testBinding
+exit
