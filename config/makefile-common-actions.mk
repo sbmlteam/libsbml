5b507e8008d23a1e507db9b706e6d4884d3c2d9a
AkiyaJouraku <ajouraku@sourceforge.net>
2008-01-24 05:22:49 +0000

    Changed LD_LIBRARY_PATH/DYLD_LIBRARY_PATH for 'libsbmlrun' shell function in order to avoid linking with incompatible libraries when 'make check' performed.

--- config/makefile-common-actions.mk
+++ config/makefile-common-actions.mk
@@ -617,14 +617,14 @@ maintainer-clean-generic:
 ifeq "$(HOST_TYPE)" "darwin"
 
   define libsbmlrun
-	DYLD_LIBRARY_PATH="$(TOP_BUILDDIR)/src:$(RUN_LDPATH):$(DYLD_LIBRARY_PATH):."; export DYLD_LIBRARY_PATH; export srcdir=.; \
+	DYLD_LIBRARY_PATH="$(TOP_BUILDDIR)/src:.:$(RUN_LDPATH):$(DYLD_LIBRARY_PATH)"; export DYLD_LIBRARY_PATH; export srcdir=.; \
 	$(1)
   endef
 
 else
 
   define libsbmlrun
-	LD_LIBRARY_PATH="$(TOP_BUILDDIR)/src:$(RUN_LDPATH):$(LD_LIBRARY_PATH):."; export LD_LIBRARY_PATH; export srcdir=.; \
+	LD_LIBRARY_PATH="$(TOP_BUILDDIR)/src:.:$(RUN_LDPATH):$(LD_LIBRARY_PATH)"; export LD_LIBRARY_PATH; export srcdir=.; \
 	$(1)
   endef
 
