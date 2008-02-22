318aa543de0b7e219c2af57de432f05dc3cea719
skeating <s.keating@ucl.ac.uk>
2008-02-22 01:37:45 +0000

    made the Utils_UnitDefinition and Utils_Unit functions static within UnitDefinition and Unit respectively this removed files Utils_Unit and Utils_UnitDefinition

--- src/bindings/swig/libsbml.h
+++ src/bindings/swig/libsbml.h
@@ -76,8 +76,6 @@
 #include <sbml/xml/XMLTokenizer.h>
 
 #include <sbml/units/FormulaUnitsData.h>
-#include <sbml/units/Utils_Unit.h>
-#include <sbml/units/Utils_UnitDefinition.h>
 
 #include <sbml/annotation/CVTerm.h>
 #include <sbml/annotation/ModelHistory.h>
