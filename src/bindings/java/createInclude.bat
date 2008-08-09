79f1d168817de434683a5f559de0daf8c87868c2
skeating <s.keating@ucl.ac.uk>
2008-08-09 11:47:21 +0000

    updated the project files to include the new compression read/write facilities

--- src/bindings/java/createInclude.bat
+++ src/bindings/java/createInclude.bat
@@ -7,6 +7,7 @@ mkdir sbml
 cd sbml
 mkdir annotation
 mkdir common
+mkdir compress
 mkdir layout
 mkdir math
 mkdir units
@@ -22,6 +23,8 @@ cd ..\math
 echo a | xcopy *.h ..\..\include\sbml\math
 cd ..\common
 echo a | xcopy *.h ..\..\include\sbml\common
+cd ..\compress
+echo a | xcopy *.h ..\..\include\sbml\compress
 cd ..\units
 echo a | xcopy *.h ..\..\include\sbml\units
 cd ..\util
