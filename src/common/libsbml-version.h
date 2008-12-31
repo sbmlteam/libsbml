9c9f1c1c6db1725a0d92b5963d13c796da2d1cbb
skeating <s.keating@ucl.ac.uk>
2008-12-31 17:37:58 +0000

    Changed the libsbml version to 3.3 in hopefully anticipation of imminent release :-)

--- src/common/libsbml-version.h
+++ src/common/libsbml-version.h
@@ -32,7 +32,7 @@
  *
  * A version string of the form "1.2.3".
  */
-#define LIBSBML_DOTTED_VERSION	"3.2.0"
+#define LIBSBML_DOTTED_VERSION	"3.3.0"
 
 /**
  * LIBSBML_VERSION:
@@ -42,14 +42,14 @@
  * libSBML is released, making it easy to use less-than and greater-than
  * comparisons when testing versions numbers.
  */
-#define LIBSBML_VERSION		30200
+#define LIBSBML_VERSION		30300
 
 /**
  * LIBSBML_VERSION_STRING:
  *
  * The numeric version as a string: version 1.2.3 becomes "10203".
  */
-#define LIBSBML_VERSION_STRING	"30200"
+#define LIBSBML_VERSION_STRING	"30300"
 
 
 BEGIN_C_DECLS
