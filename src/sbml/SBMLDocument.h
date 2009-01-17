deb7a52b0de63724cfd49be4fc91aa024dbe6a0b
mhucka <mhucka@caltech.edu>
2009-01-17 03:48:35 +0000

    Added more about consistency checking and other things.

--- src/sbml/SBMLDocument.h
+++ src/sbml/SBMLDocument.h
@@ -64,33 +64,143 @@
  * SBML data structure.  These can be accessed using the methods defined by
  * the SBase class for that purpose.
  *
- * Upon reading a model, SBMLDocument logs any problems encountered while
- * reading the model from the file or data stream.  Whether the problems
- * are warnings or errors, they are reported through a single common
- * interface involving the object class SBMLError.  The methods
- * SBMLDocument::getNumErrors(), SBMLDocument::getError() and
- * SBMLDocument::printErrors() allow callers to interact with the warnings
- * or errors found.
+ * @section checking Checking consistency and adherence to SBML specifications
  *
- * SBMLDocument also includes methods for running consistency-checking and
- * validation rules on the SBML content.  These methods assess whether the
- * SBML is legal according to basic rules listed in the SBML Level&nbsp;2
- * Versions&nbsp;2&ndash;4 specification documents.  The primary
- * interface to this facility is SBMLDocument::checkConsistency() and
- * SBMLDocument::setConsistencyChecks().
+ * One of the most important features of libSBML is its ability to perform
+ * SBML validation to ensure that a model adheres to the SBML specification
+ * for whatever Level+Version combination the model uses.  SBMLDocument
+ * provides the methods for running consistency-checking and validation
+ * rules on the SBML content.
+ *
+ * First, a brief explanation of the rationale is in order.  In libSBML
+ * versions up to and including the version 3.3.x series, the individual
+ * methods for creating and setting attributes and other components are
+ * quite lenient, and allow a caller to compose SBML entities that may not,
+ * in the end, represent valid SBML.  This allows applications the freedom
+ * to do things such as save incomplete models (which is useful when models
+ * are being developed over long periods of time), but at the same time, it
+ * means that a separate validation step is necessary when a calling
+ * program finally wants to finish a complete SBML document.
+ *
+ * The primary interface to this facility is SBMLDocument's
+ * checkInternalConsistency() and checkConsistency().  The former verifies
+ * the basic internal consistency and syntax of an SBML document, and the
+ * latter implements more elaborate validation rules (both those defined by
+ * the SBML specifications, as well as additional rules offered by
+ * libSBML).
+ *
+ * The checks performed by checkInternalConsistency() are hardwired and
+ * cannot be changed by calling programs, but the validation performed by
+ * checkConsistency() is under program control using the method
+ * setConsistencyChecks().  Applications can selectively disable specific
+ * kinds of checks that they may not be interested by calling
+ * setConsistencyChecks() with appropriate parameters.
+ *
+ * These methods have slightly different relevance depending on whether a
+ * model is beingn created programmaticaly from scratch, or whether it is
+ * being read in from a file or data stream.  The following table
+ * summarizes the possible scenarios.
+ *
+ * <center>
+ * <table border="0" class="text-table width80 normal-font alt-row-colors">
+ *  <tr style="background: lightgray; font-size: 14px;">
+ *      <th align="left" width="200">Scenario</th>
+ *      <th align="left">Relevant methods</th>
+ *  </tr>
+ * <tr><td>Creating a model from scratch</td>
+ * <td>Before writing out the model:<ol>
+ * 
+ * <li style="margin-bottom: 0.5em">Call
+ * SBMLDocument::checkInternalConsistency(), then inquire about the results by
+ * calling SBMLDocument::getNumErrors()</li>
+ * 
+ * <li style="margin-bottom: 0.5em">Call SBMLDocument::setConsistencyChecks()
+ * to configure which checks will be performed by
+ * SBMLDocument::checkConsistency()</li>
+ * 
+ * <li>Call SBMLDocument::checkConsistency(), then inquire about the results by
+ * calling SBMLDocument::getNumErrors()</li>
+ * </td>
+ * <tr><td>Reading a model from a file or data stream</td>
+ * <td>After reading the model:<ol>
+ * 
+ * <li style="margin-bottom: 0.5em">Basic consistency checks will have been
+ * performed automatically by libSBML upon reading the content&mdash;only need
+ * to inquire about the results by using SBMLDocument::getNumErrors()</li>
+ * 
+ * <li style="margin-bottom: 0.5em">Call SBMLDocument::setConsistencyChecks()
+ * to configure which checks are performed by
+ * SBMLDocument::checkConsistency()</li>
+ * 
+ * <li>Call SBMLDocument::checkConsistency(), then inquire about the results
+ * by calling SBMLDocument::getNumErrors()</li>
+ * </td>
+ * </table>
+ * </center>
+ *
+ * An example of using the consistency-checking and validation facilities
+ * is provided in this manual in the section @ref libsbml-example.
+ * 
+ * @section converting Converting documents between Levels and Versions of SBML
+ *
+ * LibSBML provides facilities for limited translation of SBML between
+ * Levels and Versions of the SBML specifications.  The method for doing is
+ * is setLevelAndVersion().  In general, models can be converted upward
+ * without difficulty (e.g., from SBML Level&nbsp;1 to Level&nbsp;2, or
+ * from an earlier Version of Level&nbsp;2 to the latest Version of
+ * Level&nbsp;2).  Sometimes models can be translated downward as well, if
+ * they do not use constructs specific to more advanced Levels of SBML.
+ *
+ * Calling setLevelAndVersion() will not @em necessarily lead to a
+ * successful conversion.  Callers must check the error log (see next
+ * section) attached to the SBMLDocument object after calling
+ * setLevelAndVersion() in order to assess whether any problems arose.
  *
  * If an application is interested in translating to a lower Level and/or
- * Version of SBML within a Level, the following methods are useful:
+ * Version of SBML within a Level, the following methods allow for prior
+ * assessment of whether there is sufficient compatibility to make a
+ * translation possible:
+ *
  * @li SBMLDocument::checkL1Compatibility(),
  * @li SBMLDocument::checkL2v1Compatibility(),
  * @li SBMLDocument::checkL2v2Compatibility(),
  * @li SBMLDocument::checkL2v3Compatibility(), and
  * @li SBMLDocument::checkL2v4Compatibility().
  * 
- * These methods allow a calling program to check the downward
- * compatibility of a model with other Levels/Versions of SBML.  At the
- * time of this writing, the most recent release of SBML is Level&nbsp;2
- * Version&nbsp;4.
+ * Some changes between Versions of SBML Level&nbsp;2 may lead to
+ * unexpected behaviors when attempting conversions in either direction.
+ * For example, SBML Level&nbsp;2 Version&nbsp;4 relaxed the requirement
+ * for consistency in units of measurement between expressions annd
+ * quantities in a model.  As a result, a model written in Version&nbsp;4,
+ * if converted to Version&nbsp;3 with no other changes, may fail
+ * validation as a Version&nbsp;3 model because Version&nbsp;3 imposed
+ * stricter requirements on unit consistency.
+ * 
+ * @section errors Error handling
+ *
+ * Upon reading a model, SBMLDocument logs any problems encountered while
+ * reading the model from the file or data stream.  The log contains
+ * objects that record diagnostic information about any notable issues that
+ * arose.  Whether the problems are warnings or errors, they are both
+ * reported through a single common interface involving the object class
+ * SBMLError.
+ *
+ * The methods getNumErrors(), getError() and printErrors() allow callers
+ * to interact with the warnings or errors logged.  Alternatively, callers
+ * may retrieve the entire log as an SBMLErrorLog object using the method
+ * getErrorLog().  The SBMLErrorLog object provides some alternative
+ * methods for interacting with the set of errors and warnings.  In either
+ * case, applications typically should first call getNumErrors() to find
+ * out if any issues have been logged after specific libSBML operations
+ * such as the ones discussed in the sections above.  If they have, then an
+ * application will should proceed to inspect the individual reports using
+ * either the direct interfaces on SBMLDocument or using the methods on the
+ * SBMLErrorLog object.
+ *
+ * An example of using the error facility is provided in this manual in the
+ * section @ref libsbml-example.
+ * 
+ * <hr style="margin: 1em auto">
  */
 
 #ifndef SBMLDocument_h
@@ -307,7 +417,7 @@ public:
    *  
    * @param version the desired Version within the SBML Level
    *
-   * @note Calling this method will not @em necessarily lead to successful
+   * @note Calling this method will not @em necessarily lead to a successful
    * conversion.  If the conversion fails, it will be logged in the error
    * list associated with this SBMLDocument.  Callers should consult
    * getNumErrors() to find out if the conversion succeeded without
@@ -458,6 +568,7 @@ public:
    * @return the number of failed checks (errors) encountered.
    *
    * @see setConsistencyChecks()
+   * @see checkInternalConsistency()
    */
   unsigned int checkConsistency ();
 
@@ -471,10 +582,15 @@ public:
    *
    * @return the number of failed checks (errors) encountered.
    *
-   * @note The consistency checks performed by this function are limited
-   * to inconsistencies that are not caught by other consistency checks.
+   * The distinction between this method and checkConsistency() is that
+   * this method reports on fundamental syntactic and structural errors
+   * that violate the XML Schema for SBML; by contrast, checkConsistency()
+   * performs more elaborate model verifications and also validation
+   * according to the validation rules written in the appendices of the
+   * SBML Level&nbsp;2 Versions&nbsp;2&ndash;4 specification documents.
    * 
    * @see setConsistencyChecks()
+   * @see checkConsistency()
    */
   unsigned int checkInternalConsistency ();
 
