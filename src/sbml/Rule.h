26cf4400718676c9942a0690787c2983f9976f11
mhucka <mhucka@caltech.edu>
2009-05-28 02:31:46 +0000

    Pulled out common text for rules into a separate file.

--- src/sbml/Rule.h
+++ src/sbml/Rule.h
@@ -37,149 +37,7 @@
  * subclasses AssignmentRule, AlgebraicRule and RateRule can be
  * instantiated directly.
  *
- * @section rules-general General summary of SBML rules
- * 
- * In SBML Level&nbsp;2, rules are separated into three subclasses for the
- * benefit of model analysis software.  The three subclasses are based on
- * the following three different possible functional forms (where @em x is
- * a variable, @em f is some arbitrary function returning a numerical
- * result, <b>V</b> is a vector of variables that does not include @em x,
- * and <b>W</b> is a vector of variables that may include @em x):
- *
- * <center>
- * <table border="0" cellpadding="0" style="font-size: small">
- * <tr><td width="120px"><em>Algebraic:</em></td><td width="250px">left-hand side is zero</td><td><em>0 = f(<b>W</b>)</em></td></tr>
- * <tr><td><em>Assignment:</em></td><td>left-hand side is a scalar:</td><td><em>x = f(<b>V</b>)</em></td></tr>
- * <tr><td><em>Rate:</em></td><td>left-hand side is a rate-of-change:</td><td><em>dx/dt = f(<b>W</b>)</em></td></tr>
- * </table>
- * </center>
- *
- * In their general form given above, there is little to distinguish
- * between @em assignment and @em algebraic rules.  They are treated as
- * separate cases for the following reasons:
- * <ul>
- * <li> @em Assignment rules can simply be evaluated to calculate
- * intermediate values for use in numerical methods.  They are statements
- * of equality that hold at all times.  (For assignments that are only
- * performed once, see InitialAssignment.)
- *
- * <li> SBML needs to place restrictions on assignment rules, for example
- * the restriction that assignment rules cannot contain algebraic loops.
- *
- * <li> Some simulators do not contain numerical solvers capable of solving
- * unconstrained algebraic equations, and providing more direct forms such
- * as assignment rules may enable those simulators to process models they
- * could not process if the same assignments were put in the form of
- * general algebraic equations;
- *
- * <li> Those simulators that @em can solve these algebraic equations make a
- * distinction between the different categories listed above; and
- *
- * <li> Some specialized numerical analyses of models may only be applicable
- * to models that do not contain @em algebraic rules.
- * </ul>
- * 
- * The approach taken to covering these cases in SBML is to define an
- * abstract Rule structure containing a subelement, "math", to hold the
- * right-hand side expression, then to derive subtypes of Rule that add
- * attributes to distinguish the cases of algebraic, assignment and rate
- * rules.  The "math" subelement must contain a MathML expression defining
- * the mathematical formula of the rule.  This MathML formula must return a
- * numerical value.  The formula can be an arbitrary expression referencing
- * the variables and other entities in an SBML model.
- *
- * Each of the three subclasses of Rule (AssignmentRule, AlgebraicRule,
- * RateRule) inherit the the "math" subelement and other fields from SBase.
- * The AssignmentRule and RateRule classes add an additional attribute,
- * "variable".  See the definitions of AssignmentRule, AlgebraicRule and
- * RateRule for details about the structure and interpretation of each one.
- * 
- * @section rule-restrictions Additional restrictions on SBML rules
- *
- * An important design goal of SBML rule semantics is to ensure that a
- * model's simulation and analysis results will not be dependent on when or
- * how often rules are evaluated.  To achieve this, SBML needs to place two
- * restrictions on rule use.  The first concerns algebraic loops in the
- * system of assignments in a model, and the second concerns overdetermined
- * systems.
- *
- * @subsection rule-loops A model must not contain algebraic loops
- *
- * The combined set of InitialAssignment, AssignmentRule and KineticLaw
- * objects in a model constitute a set of assignment statements that should
- * be considered as a whole.  (A KineticLaw object is counted as an
- * assignment because it assigns a value to the symbol contained in the
- * "id" attribute of the Reaction object in which it is defined.)  This
- * combined set of assignment statements must not contain algebraic
- * loops&mdash;dependency chains between these statements must terminate.
- * To put this more formally, consider a directed graph in which nodes are
- * assignment statements and directed arcs exist for each occurrence of an
- * SBML species, compartment or parameter symbol in an assignment
- * statement's "math" subelement.  Let the directed arcs point from the
- * statement assigning the symbol to the statements that contain the symbol
- * in their "math" subelement expressions.  This graph must be acyclic.
- *
- * SBML does not specify when or how often rules should be evaluated.
- * Eliminating algebraic loops ensures that assignment statements can be
- * evaluated any number of times without the result of those evaluations
- * changing.  As an example, consider the set of equations <em>x = x +
- * 1</em>, <em>y = z + 200</em> and <em>z = y + 100</em>.  If this set of
- * equations were interpreted as a set of assignment statements, it would
- * be invalid because the rule for <em>x</em> refers to <em>x</em>
- * (exhibiting one type of loop), and the rule for <em>y</em> refers to
- * <em>z</em> while the rule for <em>z</em> refers back to <em>y</em>
- * (exhibiting another type of loop).  Conversely, the following set of
- * equations would constitute a valid set of assignment statements: <em>x =
- * 10</em>, <em>y = z + 200</em>, and <em>z = x + 100</em>.
- *
- * @subsection rules-overdetermined A model must not be overdetermined
- *
- * An SBML model must not be overdetermined; that is, a model must not
- * define more equations than there are unknowns in a model.  An SBML model
- * that does not contain AlgebraicRule structures cannot be overdetermined.
- *
- * LibSBML 3.3 implements the static analysis procedure described in
- * Appendix D of the SBML Level&nbsp;2 Version&nbsp;4 specification for
- * assessing whether a model is overdetermined.
- *
- * (In summary, assessing whether a given continuous, deterministic,
- * mathematical model is overdetermined does not require dynamic analysis;
- * it can be done by analyzing the system of equations created from the
- * model.  One approach is to construct a bipartite graph in which one set
- * of vertices represents the variables and the other the set of vertices
- * represents the equations.  Place edges between vertices such that
- * variables in the system are linked to the equations that determine them.
- * For algebraic equations, there will be edges between the equation and
- * each variable occurring in the equation.  For ordinary differential
- * equations (such as those defined by rate rules or implied by the
- * reaction rate definitions), there will be a single edge between the
- * equation and the variable determined by that differential equation.  A
- * mathematical model is overdetermined if the maximal matchings of the
- * bipartite graph contain disconnected vertexes representing equations.
- * If one maximal matching has this property, then all the maximal
- * matchings will have this property; i.e., it is only necessary to find
- * one maximal matching.)
- *
- * <h3><a class="anchor" name="RuleType_t">RuleType_t</a></h3>
- *
- * SBML Level 1 uses a different scheme than SBML Level&nbsp;2 for
- * distinguishing rules; specifically, it uses an attribute whose value is
- * drawn from an enumeration.  LibSBML supports this using methods that
- * work with the RuleType_t enumeration.
- * 
- * <center>
- * <table width="90%" cellspacing="1" cellpadding="1" border="0" class="normal-font">
- *  <tr style="background: lightgray" class="normal-font">
- *      <td><strong>Enumerator</strong></td>
- *      <td><strong>Meaning</strong></td>
- *  </tr>
- * <tr><td><em>RULE_TYPE_RATE</em></td><td>Indicates the rule is a "rate" rule.</td>
- * <tr><td><em>RULE_TYPE_SCALAR</em></td><td>Indicates the rule is a "scalar" rule.</td>
- * <tr><td><em>RULE_TYPE_UNKNOWN</em></td><td>Indicates the rule type is unknown or not
- * yet set.</td>
- * </table>
- * </center>
- *
+ * @htmlinclude libsbml-rule-general-summary.html
  *
  * <!-- leave this next break as-is to work around some doxygen bug -->
  */ 
@@ -230,7 +88,8 @@
  * describes a method of applying this procedure to specific SBML data
  * objects.
  *
- * 
+ * @htmlinclude libsbml-rule-general-summary.html
+ *
  * <!-- leave this next break as-is to work around some doxygen bug -->
  */ 
 /**
@@ -311,7 +170,8 @@
  * parameter.  It is only a question of which value definition takes
  * precedence.
  * 
- * 
+ * @htmlinclude libsbml-rule-general-summary.html
+ *
  * <!-- leave this next break as-is to work around some doxygen bug -->
  */ 
 /**
@@ -373,6 +233,8 @@
  * one assignment rule for the same variable or both an assignment rule and
  * a rate rule for the same variable.
  * 
+ * @htmlinclude libsbml-rule-general-summary.html
+ *
  * <!-- leave this next break as-is to work around some doxygen bug -->
  */ 
 /**
