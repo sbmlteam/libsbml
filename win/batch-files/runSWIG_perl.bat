d35f99a9bc06f8b7d390ef29f340f99b2e08de88
skeating <s.keating@ucl.ac.uk>
2010-07-20 14:16:47 +0000

    added files to run swig for building perl binding

---
+++ win/batch-files/runSWIG_perl.bat
@@ -0,0 +1,8 @@
+REM run swig for perl
+cd ..
+cd ..
+cd src
+cd bindings
+cd perl
+C:/libsbml_trunk/src/bindings/swig/swigdoc.py perl -I../.. -D../../../docs/src ../swig/libsbml.i LibSBML.pod
+C:/swigwin-1.3.40/swig.exe -I../swig  -c++ -DUSE_LAYOUT -perl5 -proxy -I../../../include -o libsbml_perl_wrap.cpp Libsbml.i
