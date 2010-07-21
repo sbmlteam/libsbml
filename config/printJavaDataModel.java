4d369ac3195702e25f0870baf6eb473588c81871
mhucka <mhucka@caltech.edu>
2010-07-21 03:23:52 +0000

    May as well put on the usual file header, even though this is such a trivial piece of code.

--- config/printJavaDataModel.java
+++ config/printJavaDataModel.java
@@ -1,3 +1,26 @@
+/*
+ * @file    printJavaDataModel.java
+ * @brief   Trivial program to print the value of sun.arch.data.model.
+ * @author  Michael Hucka
+ *
+ * $Id$
+ * $HeadURL$
+ *
+ *<!---------------------------------------------------------------------------
+ * This file is part of libSBML.  Please visit http://sbml.org for more
+ * information about SBML, and the latest version of libSBML.
+ *
+ * Copyright 2005-2010 California Institute of Technology.
+ * Copyright 2002-2005 California Institute of Technology and
+ *                     Japan Science and Technology Corporation.
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation.  A copy of the license agreement is provided
+ * in the file named "LICENSE.txt" included with this software distribution
+ * and also available online as http://sbml.org/software/libsbml/license.html
+ *----------------------------------------------------------------------- -->*/
+
 public class printJavaDataModel
 {
     public static void main(String[] args)
