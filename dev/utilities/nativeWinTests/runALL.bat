b5049b91c3a2e152d77de6a1cf28e652442ea1ef
skeating <s.keating@ucl.ac.uk>
2010-07-28 11:20:51 +0000

    added files to clean and run all tests

---
+++ dev/utilities/nativeWinTests/runALL.bat
@@ -0,0 +1,25 @@
+echo off
+cd ..
+cd ..
+cd ..
+cd win
+cd bin
+testEchoSBML C:\Biomodels\release_26January2010_sbmls\curated
+testValidateSBML C:\Biomodels\release_26January2010_sbmls\curated
+cd ..
+cd ..
+cd dev
+cd utilities
+cd nativeWinTests
+cd csharp
+testCSharp
+cd ..
+cd java
+testJava
+cd ..
+cd perl
+testPerl
+cd ..
+cd python
+testPython
+cd ..
