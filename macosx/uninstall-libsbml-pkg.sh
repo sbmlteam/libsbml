7cb067e00fa3e9b7a299e0990d668b1422e7d670
mhucka <mhucka@caltech.edu>
2010-08-24 02:03:21 +0000

    Copyright update + minor text edit.  Also turned on svn keyword expansion.

--- macosx/uninstall-libsbml-pkg.sh
+++ macosx/uninstall-libsbml-pkg.sh
@@ -11,15 +11,16 @@
 # This file is part of libSBML.  Please visit http://sbml.org for more
 # information about SBML, and the latest version of libSBML.
 #
-# Copyright 2009 California Institute of Technology.
-#
+# Copyright 2005-2010 California Institute of Technology.
+# Copyright 2002-2005 California Institute of Technology and
+#                     Japan Science and Technology Corporation.
+# 
 # This library is free software; you can redistribute it and/or modify it
 # under the terms of the GNU Lesser General Public License as published by
 # the Free Software Foundation.  A copy of the license agreement is provided
 # in the file named "LICENSE.txt" included with this software distribution
 # and also available online as http://sbml.org/software/libsbml/license.html
 #----------------------------------------------------------------------- -->*/
-#
 
 set -e
 
@@ -31,7 +32,7 @@ MYPATH=uninstall-libsbml-pkg.sh
 DEBUG=
 
 if [ ${EUID} != 0 ] ; then
-  echo "Please run this shell script as root user."
+  echo "Please run this shell script as the user 'root'."
   exit 1
 fi	
 
