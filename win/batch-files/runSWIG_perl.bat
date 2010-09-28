8ad02f504f0feb5e30af32cd2df3436eb2f3aa60
skeating <s.keating@ucl.ac.uk>
2010-09-28 10:39:48 +0000

    use swigwin-2.0.0

--- win/batch-files/runSWIG_perl.bat
+++ win/batch-files/runSWIG_perl.bat
@@ -5,4 +5,4 @@ cd src
 cd bindings
 cd perl
 C:/libsbml_trunk/src/bindings/swig/swigdoc.py perl -I../.. -D../../../docs/src ../swig/libsbml.i LibSBML.pod
-C:/swigwin-1.3.40/swig.exe -I../swig  -c++ -DUSE_LAYOUT -perl5 -proxy -I../../../include -o libsbml_perl_wrap.cpp Libsbml.i
+C:/swigwin-2.0.0/swig.exe -I../swig  -c++ -DUSE_LAYOUT -perl5 -proxy -I../../../include -o libsbml_perl_wrap.cpp Libsbml.i
