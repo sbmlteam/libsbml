454560b7cc47166d8f8a75b59e29eca9b01eac3c
mhucka <mhucka@caltech.edu>
2011-02-15 06:24:57 +0000

    New example file demonstrating how to query the JAR file for libsbml's version number.

---
+++ examples/java/printLibSBMLVersion.java
@@ -0,0 +1,70 @@
+/**
+ * @file    printLibSBMLVersion.java
+ * @brief   Reads the JAR file manifest and prints libSBML's version info
+ * @author  Michael Hucka
+ *
+ * $Id$
+ * $URL$
+ *
+ * This file is part of libSBML.  Please visit http://sbml.org for more
+ * information about SBML, and the latest version of libSBML.
+ */
+
+public class printLibSBMLVersion
+{
+  public static void main (String[] args)
+  {
+    Package p = Package.getPackage("org.sbml.libsbml");
+    if (p != null)
+    {
+      System.out.println("Package name: " + p.getImplementationTitle());
+      System.out.println("Package version: " + p.getImplementationVersion());
+    }
+    else
+    {
+      System.err.println("Unable to get package org.sbml.libsbml");
+    }
+  }
+
+  /**
+   * Loads the SWIG-generated libSBML Java module when this class is
+   * loaded, or reports a sensible diagnostic message about why it failed.
+   */
+  static
+  {
+    String varname;
+
+    if (System.getProperty("mrj.version") != null)
+      varname = "DYLD_LIBRARY_PATH";	// We're on a Mac.
+    else
+      varname = "LD_LIBRARY_PATH";	// We're not on a Mac.
+
+    try
+    {
+      System.loadLibrary("sbmlj");
+      // For extra safety, check that the jar file is in the classpath.
+      Class.forName("org.sbml.libsbml.libsbml");
+    }
+    catch (UnsatisfiedLinkError e)
+    {
+      System.err.println("Error: could not link with the libSBML library."+
+			 "  It is likely\nyour " + varname +
+			 " environment variable does not include\nthe"+
+			 " directory containing the libsbml library file.");
+      System.exit(1);
+    }
+    catch (ClassNotFoundException e)
+    {
+      System.err.println("Error: unable to load the file libsbmlj.jar."+
+			 "  It is likely\nyour " + varname + " environment"+
+			 " variable or CLASSPATH variable\ndoes not include"+
+			 " the directory containing the libsbmlj.jar file.");
+      System.exit(1);
+    }
+    catch (SecurityException e)
+    {
+      System.err.println("Could not load the libSBML library files due to a"+
+			 " security exception.");
+    }
+  }
+}
