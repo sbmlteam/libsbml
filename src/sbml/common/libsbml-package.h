1d4b565c2fc573c27ae6fa01ba21f9960e0876d4
skeating <s.keating@ucl.ac.uk>
2011-12-17 11:06:53 +0000

    updated VERSION.txt

--- src/sbml/common/libsbml-package.h
+++ src/sbml/common/libsbml-package.h
@@ -52,11 +52,11 @@
 #define PACKAGE_NAME "libSBML"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "libSBML 5.2.0"
+#define PACKAGE_STRING "libSBML 5.3.0"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "libsbml"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "5.2.0"
+#define PACKAGE_VERSION "5.3.0"
 
