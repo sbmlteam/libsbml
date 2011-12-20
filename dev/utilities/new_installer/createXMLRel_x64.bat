3b349de3542173b724476974f026a730940eccad
fbergmann <fbergman@caltech.edu>
2011-12-20 03:13:08 +0000

    - add the new examples to the install script

--- dev/utilities/new_installer/createXMLRel_x64.bat
+++ dev/utilities/new_installer/createXMLRel_x64.bat
@@ -85,52 +85,87 @@ copy "%LIBSBML_ROOT%\examples\README.txt" README.txt
 rem examples/c directory
 
 cd c
+copy %LIBSBML_ROOT%\examples\c\addCVTerms.c addCVTerms.c
+copy %LIBSBML_ROOT%\examples\c\addModelHistory.c addModelHistory.c
+copy %LIBSBML_ROOT%\examples\c\addingEvidenceCodes_1.c addingEvidenceCodes_1.c
+copy %LIBSBML_ROOT%\examples\c\addingEvidenceCodes_2.c addingEvidenceCodes_2.c
+copy %LIBSBML_ROOT%\examples\c\appendAnnotation.c appendAnnotation.c
 copy %LIBSBML_ROOT%\examples\c\convertSBML.c convertSBML.c
+copy %LIBSBML_ROOT%\examples\c\createExampleSBML.c createExampleSBML.c
 copy %LIBSBML_ROOT%\examples\c\drawMath.c drawMath.c
+copy %LIBSBML_ROOT%\examples\c\echoSBML.c echoSBML.c
 copy %LIBSBML_ROOT%\examples\c\evaluateMath.c evaluateMath.c
 copy %LIBSBML_ROOT%\examples\c\FormulaGraphvizFormatter.h FormulaGraphvizFormatter.h
+copy %LIBSBML_ROOT%\examples\c\printAnnotation.c printAnnotation.c 
 copy %LIBSBML_ROOT%\examples\c\printMath.c printMath.c
+copy %LIBSBML_ROOT%\examples\c\printNotes.c printNotes.c
+copy %LIBSBML_ROOT%\examples\c\printRegisteredPackages.c printRegisteredPackages.c
 copy %LIBSBML_ROOT%\examples\c\printSBML.c printSBML.c
+copy %LIBSBML_ROOT%\examples\c\printUnits.c printUnits.c
+copy %LIBSBML_ROOT%\examples\c\printSupported.c printSupported.c
 copy %LIBSBML_ROOT%\examples\c\readSBML.c readSBML.c
 copy %LIBSBML_ROOT%\examples\c\translateMath.c translateMath.c
+copy %LIBSBML_ROOT%\examples\c\unsetAnnotation.c unsetAnnotation.c
+copy %LIBSBML_ROOT%\examples\c\unsetNotes.c unsetNotes.c
 copy %LIBSBML_ROOT%\examples\c\util.c util.c
-copy %LIBSBML_ROOT%\examples\c\validateSBML.c validateSBML.c
 copy %LIBSBML_ROOT%\examples\c\util.h util.h
-copy %LIBSBML_ROOT%\examples\c\echoSBML.c echoSBML.c
+copy %LIBSBML_ROOT%\examples\c\validateSBML.c validateSBML.c
 cd ..
 
 rem examples/c++ directory
 
 cd c++
+copy "%LIBSBML_ROOT%\examples\c++\addCustomValidator.cpp" addCustomValidator.cpp
+copy "%LIBSBML_ROOT%\examples\c++\addCVTerms.cpp" addCVTerms.cpp
+copy "%LIBSBML_ROOT%\examples\c++\addingEvidenceCodes_1.cpp" addingEvidenceCodes_1.cpp
+copy "%LIBSBML_ROOT%\examples\c++\addingEvidenceCodes_2.cpp" addingEvidenceCodes_2.cpp
+copy "%LIBSBML_ROOT%\examples\c++\addModelHistory.cpp" addModelHistory.cpp
+copy "%LIBSBML_ROOT%\examples\c++\appendAnnotation.cpp" appendAnnotation.cpp
+copy "%LIBSBML_ROOT%\examples\c++\callExternalValidator.cpp" callExternalValidator.cpp
+copy "%LIBSBML_ROOT%\examples\c++\convertSBML.cpp" convertSBML.cpp
+copy "%LIBSBML_ROOT%\examples\c++\createExampleSBML.cpp" createExampleSBML.cpp
+copy "%LIBSBML_ROOT%\examples\c++\echoSBML.cpp" echoSBML.cpp
+copy "%LIBSBML_ROOT%\examples\c++\printAnnotation.cpp" printAnnotation.cpp
 copy "%LIBSBML_ROOT%\examples\c++\printMath.cpp" printMath.cpp
+copy "%LIBSBML_ROOT%\examples\c++\printNotes.cpp" printNotes.cpp
+copy "%LIBSBML_ROOT%\examples\c++\printRegisteredPackages.cpp" printRegisteredPackages.cpp
 copy "%LIBSBML_ROOT%\examples\c++\printSBML.cpp" printSBML.cpp
+copy "%LIBSBML_ROOT%\examples\c++\printSupported.cpp" printSupported.cpp
+copy "%LIBSBML_ROOT%\examples\c++\printUnits.cpp" printUnits.cpp
 copy "%LIBSBML_ROOT%\examples\c++\readSBML.cpp" readSBML.cpp
 copy "%LIBSBML_ROOT%\examples\c++\translateMath.cpp" translateMath.cpp
-copy "%LIBSBML_ROOT%\examples\c++\util.c" util.c
-copy "%LIBSBML_ROOT%\examples\c++\validateSBML.cpp" validateSBML.cpp
-copy "%LIBSBML_ROOT%\examples\c++\util.h" util.h
-copy "%LIBSBML_ROOT%\examples\c++\addCVTerms.cpp" addCVTerms.cpp
-copy "%LIBSBML_ROOT%\examples\c++\addModelHistory.cpp" addModelHistory.cpp
-copy "%LIBSBML_ROOT%\examples\c++\echoSBML.cpp" echoSBML.cpp
-copy "%LIBSBML_ROOT%\examples\c++\printUnits.cpp" printUnits.cpp
-copy "%LIBSBML_ROOT%\examples\c++\appendAnnotation.cpp" appendAnnotation.cpp
-copy "%LIBSBML_ROOT%\examples\c++\convertSBML.cpp" convertSBML.cpp
-copy "%LIBSBML_ROOT%\examples\c++\printAnnotation.cpp" printAnnotation.cpp
-copy "%LIBSBML_ROOT%\examples\c++\printNotes.cpp" printNotes.cpp
 copy "%LIBSBML_ROOT%\examples\c++\unsetAnnotation.cpp" unsetAnnotation.cpp
 copy "%LIBSBML_ROOT%\examples\c++\unsetNotes.cpp" unsetNotes.cpp
-copy "%LIBSBML_ROOT%\examples\c++\createExampleSBML.cpp" createExampleSBML.cpp
+copy "%LIBSBML_ROOT%\examples\c++\util.h" util.h
+copy "%LIBSBML_ROOT%\examples\c++\util.c" util.c
+copy "%LIBSBML_ROOT%\examples\c++\validateSBML.cpp" validateSBML.cpp
 cd ..
 
 rem examples/java directory
 
 cd java
+copy %LIBSBML_ROOT%\examples\java\addCustomValidator.java addCustomValidator.java
+copy %LIBSBML_ROOT%\examples\java\addCVTerms.java addCVTerms.java
+copy %LIBSBML_ROOT%\examples\java\addingEvidenceCodes_1.java addingEvidenceCodes_1.java
+copy %LIBSBML_ROOT%\examples\java\addingEvidenceCodes_2.java addingEvidenceCodes_2.java
+copy %LIBSBML_ROOT%\examples\java\addModelHistory.java addModelHistory.java
+copy %LIBSBML_ROOT%\examples\java\appendAnnotation.java appendAnnotation.java
+copy %LIBSBML_ROOT%\examples\java\callExternalValidator.java callExternalValidator.java
 copy %LIBSBML_ROOT%\examples\java\convertSBML.java convertSBML.java
+copy %LIBSBML_ROOT%\examples\java\createExampleSBML.java createExampleSBML.java
+copy %LIBSBML_ROOT%\examples\java\echoSBML.java echoSBML.java
 copy %LIBSBML_ROOT%\examples\java\evaluateMath.java evaluateMath.java
+copy %LIBSBML_ROOT%\examples\java\printAnnotation.java printAnnotation.java
 copy %LIBSBML_ROOT%\examples\java\printMath.java printMath.java
+copy %LIBSBML_ROOT%\examples\java\printNotes.java printNotes.java
+copy %LIBSBML_ROOT%\examples\java\printRegisteredPackages.java printRegisteredPackages.java
 copy %LIBSBML_ROOT%\examples\java\printSBML.java printSBML.java
+copy %LIBSBML_ROOT%\examples\java\printsupported.java printsupported.java
+copy %LIBSBML_ROOT%\examples\java\printUnits.java printUnits.java
 copy %LIBSBML_ROOT%\examples\java\readSBML.java readSBML.java
 copy %LIBSBML_ROOT%\examples\java\translateMath.java translateMath.java
+copy %LIBSBML_ROOT%\examples\java\unsetAnnotation.java unsetAnnotation.java
+copy %LIBSBML_ROOT%\examples\java\unsetNotes.java unsetNotes.java
 copy %LIBSBML_ROOT%\examples\java\validateSBML.java validateSBML.java
 copy %LIBSBML_ROOT%\examples\java\README.txt README.txt
 cd ..
@@ -138,22 +173,47 @@ cd ..
 rem examples/perl directory
 
 cd perl
+copy %LIBSBML_ROOT%\examples\perl\addCustomValidator.pl addCustomValidator.pl
+copy %LIBSBML_ROOT%\examples\perl\addCVTerms.pl addCVTerms.pl
+copy %LIBSBML_ROOT%\examples\perl\addingEvidenceCodes_1.pl addingEvidenceCodes_1.pl
+copy %LIBSBML_ROOT%\examples\perl\addingEvidenceCodes_2.pl addingEvidenceCodes_2.pl
+copy %LIBSBML_ROOT%\examples\perl\addModelHistory.pl addModelHistory.pl
+copy %LIBSBML_ROOT%\examples\perl\appendAnnotation.pl appendAnnotation.pl
 copy %LIBSBML_ROOT%\examples\perl\convertSBML.pl convertSBML.pl
+copy %LIBSBML_ROOT%\examples\perl\echoSBML.pl echoSBML.pl
 copy %LIBSBML_ROOT%\examples\perl\evaluateMath.pl evaluateMath.pl
+copy %LIBSBML_ROOT%\examples\perl\extractReactionInfo.pl extractReactionInfo.pl
+copy %LIBSBML_ROOT%\examples\perl\extractReactions.pl extractReactions.pl
+copy %LIBSBML_ROOT%\examples\perl\printAnnotation.pl printAnnotation.pl
 copy %LIBSBML_ROOT%\examples\perl\printMath.pl printMath.pl
+copy %LIBSBML_ROOT%\examples\perl\printNotes.pl printNotes.pl
+copy %LIBSBML_ROOT%\examples\perl\printRegisteredPackages.pl printRegisteredPackages.pl
 copy %LIBSBML_ROOT%\examples\perl\printSBML.pl printSBML.pl
+copy %LIBSBML_ROOT%\examples\perl\printsupported.pl printsupported.pl
+copy %LIBSBML_ROOT%\examples\perl\printUnits.pl printUnits.pl
 copy %LIBSBML_ROOT%\examples\perl\readSBML.pl readSBML.pl
 copy %LIBSBML_ROOT%\examples\perl\translateMath.pl translateMath.pl
+copy %LIBSBML_ROOT%\examples\perl\unsetAnnotation.pl unsetAnnotation.pl
+copy %LIBSBML_ROOT%\examples\perl\unsetNotes.pl unsetNotes.pl
 copy %LIBSBML_ROOT%\examples\perl\validateSBML.pl validateSBML.pl
 cd ..
 
 rem examples/layout directory
 
 cd layout
-copy %LIBSBML_ROOT%\examples\layout\example1.py example1.py
+copy %LIBSBML_ROOT%\examples\layout\addLayout.cpp addLayout.cpp
+copy %LIBSBML_ROOT%\examples\layout\example1-L3.cpp example1-L3.cpp
+copy %LIBSBML_ROOT%\examples\layout\example1-L3.cs example1-L3.cs
+copy %LIBSBML_ROOT%\examples\layout\example1-L3.pl example1-L3.pl
+copy %LIBSBML_ROOT%\examples\layout\example1-L3.py example1-L3.py
+copy %LIBSBML_ROOT%\examples\layout\example1-L3.rb example1-L3.rb
 copy %LIBSBML_ROOT%\examples\layout\example1.cpp example1.cpp
 copy %LIBSBML_ROOT%\examples\layout\example1.java example1.java
+copy %LIBSBML_ROOT%\examples\layout\example1.py example1.py
+copy %LIBSBML_ROOT%\examples\layout\example1_L3.java example1_L3.java
+copy %LIBSBML_ROOT%\examples\layout\example2-L3.cpp example2-L3.cpp
 copy %LIBSBML_ROOT%\examples\layout\example2.cpp example2.cpp
+copy %LIBSBML_ROOT%\examples\layout\example3-L3.cpp example3-L3.cpp
 copy %LIBSBML_ROOT%\examples\layout\example3.cpp example3.cpp
 copy %LIBSBML_ROOT%\examples\layout\layout2svg.xsl layout2svg.xsl
 cd ..
@@ -161,14 +221,53 @@ cd ..
 rem examples/csharp directory
 
 cd csharp
+copy %LIBSBML_ROOT%\examples\csharp\addCustomValidator.cs addCustomValidator.cs
+copy %LIBSBML_ROOT%\examples\csharp\addCVTerms.cs addCVTerms.cs
+copy %LIBSBML_ROOT%\examples\csharp\addingEvidenceCodes_1.cs addingEvidenceCodes_1.cs
+copy %LIBSBML_ROOT%\examples\csharp\addingEvidenceCodes_2.cs addingEvidenceCodes_2.cs
+copy %LIBSBML_ROOT%\examples\csharp\addModelHistory.cs addModelHistory.cs
+copy %LIBSBML_ROOT%\examples\csharp\appendAnnotation.cs appendAnnotation.cs
+copy %LIBSBML_ROOT%\examples\csharp\callExternalValidator.cs callExternalValidator.cs
+copy %LIBSBML_ROOT%\examples\csharp\convertSBML.cs convertSBML.cs
+copy %LIBSBML_ROOT%\examples\csharp\createExampleSBML.cs createExampleSBML.cs
 copy %LIBSBML_ROOT%\examples\csharp\echoSBML.cs echoSBML.cs
+copy %LIBSBML_ROOT%\examples\csharp\printAnnotation.cs printAnnotation.cs
+copy %LIBSBML_ROOT%\examples\csharp\printMath.cs printMath.cs
+copy %LIBSBML_ROOT%\examples\csharp\printNotes.cs printNotes.cs
+copy %LIBSBML_ROOT%\examples\csharp\printRegisteredPackages.cs printRegisteredPackages.cs
+copy %LIBSBML_ROOT%\examples\csharp\printSBML.cs printSBML.cs
+copy %LIBSBML_ROOT%\examples\csharp\printsupported.cs printsupported.cs
+copy %LIBSBML_ROOT%\examples\csharp\printUnits.cs printUnits.cs
+copy %LIBSBML_ROOT%\examples\csharp\readSBML.cs readSBML.cs
+copy %LIBSBML_ROOT%\examples\csharp\translateMath.cs translateMath.cs
+copy %LIBSBML_ROOT%\examples\csharp\unsetAnnotation.cs unsetAnnotation.cs
+copy %LIBSBML_ROOT%\examples\csharp\unsetNotes.cs unsetNotes.cs
 copy %LIBSBML_ROOT%\examples\csharp\validateSBML.cs validateSBML.cs
 cd ..
 
 rem examples/python directory
 
 cd python
+copy %LIBSBML_ROOT%\examples\python\addCustomValidator.py addCustomValidator.py
+copy %LIBSBML_ROOT%\examples\python\addCVTerms.py addCVTerms.py
+copy %LIBSBML_ROOT%\examples\python\addingEvidenceCodes_1.py addingEvidenceCodes_1.py
+copy %LIBSBML_ROOT%\examples\python\addingEvidenceCodes_2.py addingEvidenceCodes_2.py
+copy %LIBSBML_ROOT%\examples\python\addModelHistory.py addModelHistory.py
+copy %LIBSBML_ROOT%\examples\python\appendAnnotation.py appendAnnotation.py
+copy %LIBSBML_ROOT%\examples\python\callExternalValidator.py callExternalValidator.py
+copy %LIBSBML_ROOT%\examples\python\convertSBML.py convertSBML.py
+copy %LIBSBML_ROOT%\examples\python\createExampleSBML.py createExampleSBML.py
 copy %LIBSBML_ROOT%\examples\python\echoSBML.py echoSBML.py
+copy %LIBSBML_ROOT%\examples\python\printAnnotation.py printAnnotation.py
+copy %LIBSBML_ROOT%\examples\python\printMath.py printMath.py
+copy %LIBSBML_ROOT%\examples\python\printNotes.py printNotes.py
+copy %LIBSBML_ROOT%\examples\python\printRegisteredPackages.py printRegisteredPackages.py
+copy %LIBSBML_ROOT%\examples\python\printSBML.py printSBML.py
+copy %LIBSBML_ROOT%\examples\python\printsupported.py printsupported.py
+copy %LIBSBML_ROOT%\examples\python\printUnits.py printUnits.py
+copy %LIBSBML_ROOT%\examples\python\translateMath.py translateMath.py
+copy %LIBSBML_ROOT%\examples\python\unsetAnnotation.py unsetAnnotation.py
+copy %LIBSBML_ROOT%\examples\python\unsetNotes.py unsetNotes.py
 copy %LIBSBML_ROOT%\examples\python\validateSBML.py validateSBML.py
 cd ..
 
