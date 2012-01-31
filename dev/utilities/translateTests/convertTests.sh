3a1555055bee99008c448a714428502b8efba64b
fbergmann <fbergman@caltech.edu>
2012-01-31 03:49:44 +0000

    - added convenience function for translating tests, to be invoked from the libsbml root dir

---
+++ dev/utilities/translateTests/convertTests.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+DIRECTORY=$(cd `dirname $0` && pwd)
+BASEDIR=$DIRECTORY/../../../
+cd $BASEDIR
+PERL=perl
+
+FILE=$1
+SUBDIR=
+case "$FILE" in
+  *math*)
+    SUBDIR=math
+    ;;
+  *annotation*)
+    SUBDIR=annotation
+    ;;
+  *xml*)
+    SUBDIR=xml
+    ;;
+  *sbml*)
+    SUBDIR=sbml
+    ;;
+esac
+
+if [ "$FILE" = "" ]
+then
+echo "usage: convertTests subdir testfile"
+echo ""
+echo "Converts libSBML tests into Perl, Ruby, CSharp and Java"
+echo "subdir has to be one of: math, sbml, annotation, xml"
+echo ""
+fi
+
+if [ ! -f "./configure" ];
+then
+  echo "Please run this script from the libsbml root directory."
+  exit 1
+fi
+if [ ! -f $FILE ];
+then
+    echo "File not found!"
+    exit 1
+fi
+
+$PERL  ./dev/utilities/translateTests/translateTests.pl -o ./src/bindings/python/test/$SUBDIR -p $FILE
+$PERL  ./dev/utilities/translateTests/translateTests.pl -o ./src/bindings/ruby/test/$SUBDIR -r $FILE
+$PERL  ./dev/utilities/translateTests/translateTests.pl -o ./src/bindings/csharp/test/$SUBDIR -c $FILE
+$PERL  ./dev/utilities/translateTests/translateTests.pl -o  ./src/bindings/java/test/org/sbml/libsbml/test/$SUBDIR -j $FILE
