782da8d0229693a3a4ea6c642db05ad7c612bbe5
skeating <s.keating@ucl.ac.uk>
2012-12-16 15:42:52 +0000

    the code for disabling a function internally removed an package from the set of namespaces even if it were an Unknown package. This meant that if you internally disabled an unknowm package and then wrote out the document you would still get the required attribute for the package with an unbound prefix.
    I altered the code so if you were removing an unknown namespace you also removed the recird of the associated required attribute

--- src/sbml/SBMLDocument.cpp
+++ src/sbml/SBMLDocument.cpp
@@ -1715,6 +1715,16 @@ SBMLDocument::enablePackageInternal(const std::string& pkgURI, const std::string
 //    {
 //      xmlns->remove(xmlns->getIndex(pkgURI));
 //    }
+
+    for (unsigned int i = 0; i < mRequiredAttrOfUnknownPkg.getLength(); i++)
+    {
+      if (pkgURI == mRequiredAttrOfUnknownPkg.getURI(i)
+        && pkgPrefix == mRequiredAttrOfUnknownPkg.getPrefix(i))
+      {
+        mRequiredAttrOfUnknownPkg.remove(i);
+        break;
+      }
+    }
   }
 
   if (mModel)
