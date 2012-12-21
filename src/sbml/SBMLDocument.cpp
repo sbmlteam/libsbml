7848b579f5041e385b7eac604881a3da3204c9dd
skeating <s.keating@ucl.ac.uk>
2012-12-21 13:32:19 +0000

    fixed signed unsigned mismatch

--- src/sbml/SBMLDocument.cpp
+++ src/sbml/SBMLDocument.cpp
@@ -1724,7 +1724,7 @@ SBMLDocument::enablePackageInternal(const std::string& pkgURI, const std::string
 //      xmlns->remove(xmlns->getIndex(pkgURI));
 //    }
 
-    for (unsigned int i = 0; i < mRequiredAttrOfUnknownPkg.getLength(); i++)
+    for (int i = 0; i < mRequiredAttrOfUnknownPkg.getLength(); i++)
     {
       if (pkgURI == mRequiredAttrOfUnknownPkg.getURI(i)
         && pkgPrefix == mRequiredAttrOfUnknownPkg.getPrefix(i))
