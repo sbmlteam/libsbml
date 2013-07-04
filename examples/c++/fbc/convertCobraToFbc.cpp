c35afdb0d41932bb63e4934a22348cf5504e9294
skeating <s.keating@ucl.ac.uk>
2013-07-04 14:12:47 +0000

    SVN MOVE example files

---
+++ examples/c++/fbc/convertCobraToFbc.cpp
@@ -0,0 +1,54 @@
+/**
+* @file    convertCobraToFbc.cpp
+* @brief   Convert COBRA L2 to L3 with FBC
+* @author  Frank T. Bergmann
+* 
+* This file is part of libSBML.  Please visit http://sbml.org for more
+* information about SBML, and the latest version of libSBML.
+*/
+
+
+#include <iostream>
+#include <sbml/SBMLTypes.h>
+#include <sbml/conversion/ConversionProperties.h>
+
+using namespace std;
+LIBSBML_CPP_NAMESPACE_USE
+
+  int
+  main (int argc, char* argv[])
+{
+  if (argc != 3)
+  {
+    cout << endl << "Usage: convertCobraToFbc input-filename output-filename"
+      << endl << endl;
+    return 2;
+  }
+
+  SBMLDocument *d = readSBML(argv[1]);
+  if ( d->getNumErrors(LIBSBML_SEV_ERROR) > 0)
+  {
+    d->printErrors();
+  }
+  else
+  {
+    /* create a new conversion properties structure */
+    ConversionProperties props;
+
+    /* add an option that we want to strip a given package */
+    props.addOption("convert cobra", true, "Convert Cobra model to FBC");
+
+
+    /* perform the conversion */
+	int result = d->convert(props);
+    if (result != LIBSBML_OPERATION_SUCCESS)
+    {
+      cout<< "conversion failed ... " << endl;
+      return 3;
+    }
+
+    writeSBML(d, argv[2]);
+  }
+
+  return 0;
+}
