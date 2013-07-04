6fc9e0d57af6082de6c01c6a62d15c5caf4a2a24
skeating <s.keating@ucl.ac.uk>
2013-07-04 14:17:54 +0000

    SVN MOVE example files

---
+++ examples/ruby/fbc/convertCobraToFbc.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+#
+# @file    convertCobraToFbc.rb
+# @brief   Convert COBRA L2 to L3 with FBC
+# @author  Frank T. Bergmann
+# 
+# This file is part of libSBML.  Please visit http://sbml.org for more
+# information about SBML, and the latest version of libSBML.
+
+require 'libSBML'
+
+if ARGV.size != 2
+  puts "Usage: convertCobraToFbc input-filename output-filename"
+  exit(2)
+end
+
+d = LibSBML::readSBML(ARGV[0])
+if (d.getNumErrors() > 0)
+	d.printErrors
+else
+	props = LibSBML::ConversionProperties.new
+	props.addOption("convert cobra", true, "Convert Cobra model")
+	if d.convert(props) != 0
+		puts "[Error] conversion failed..."
+		exit(3)
+	end
+	LibSBML::writeSBML(d,ARGV[1])
+end
+
