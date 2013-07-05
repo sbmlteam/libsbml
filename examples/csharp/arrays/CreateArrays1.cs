bc11df4c51551ed23e094557ccbbadac19a316db
skeating <s.keating@ucl.ac.uk>
2013-07-05 13:16:40 +0000

    SVN MOVE examples files

---
+++ examples/csharp/arrays/CreateArrays1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using libsbmlcs;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var arraysNs = new ArraysPkgNamespaces();
+        var doc = new SBMLDocument(arraysNs);
+        doc.setPackageRequired("arrays", true);
+        var model = doc.createModel();
+
+        // create compartment
+        var comp = model.createCompartment();
+        comp.setMetaId("dd");
+        comp.setId("s");
+        comp.setConstant(true);
+
+        // set dimensions
+        var compPlugin = (ArraysCompartmentPlugin)comp.getPlugin("arrays");
+        var dim = compPlugin.createDimension();
+        dim.setId("i");
+        dim.setSize("n");
+
+        // create species
+        var species = model.createSpecies();
+        species.setId("A");
+        species.setCompartment("s");
+        species.setHasOnlySubstanceUnits(false);
+        species.setBoundaryCondition(false);
+        species.setConstant(false);
+
+        var splugin = (ArraysSpeciesPlugin)species.getPlugin("arrays");
+        dim = splugin.createDimension();
+        dim.setId("i");
+        dim.setSize("n");
+
+        species = model.createSpecies();
+        species.setId("B");
+        species.setCompartment("s");
+        species.setHasOnlySubstanceUnits(false);
+        species.setBoundaryCondition(false);
+        species.setConstant(false);
+
+        splugin = (ArraysSpeciesPlugin)species.getPlugin("arrays");
+        dim = splugin.createDimension();
+        dim.setId("i");
+        dim.setSize("n");
+
+        species = model.createSpecies();
+        species.setId("C");
+        species.setCompartment("s");
+        species.setHasOnlySubstanceUnits(false);
+        species.setBoundaryCondition(false);
+        species.setConstant(false);
+
+        splugin = (ArraysSpeciesPlugin)species.getPlugin("arrays");
+        dim = splugin.createDimension();
+        dim.setId("i");
+        dim.setSize("n");
+
+        // create parameter
+        var param = model.createParameter();
+        param.setId("n");
+        param.setValue(100);
+        param.setConstant(true);
+
+        // create reaction
+        var reaction = model.createReaction();
+        reaction.setId("reaction1");
+        reaction.setReversible(false);
+        reaction.setFast(false);
+
+        var reactionPlugin = (ArraysReactionPlugin)reaction.getPlugin("arrays");
+        dim = reactionPlugin.createDimension();
+        dim.setId("i");
+        dim.setSize("n");
+
+        var speciesRef = reaction.createReactant();
+        speciesRef.setSpecies("A");
+        speciesRef.setConstant(false);
+        var refPlugin = (ArraysSpeciesReferencePlugin)speciesRef.getPlugin("arrays");
+        var index = refPlugin.createIndex();
+		var ast = new NewASTNode(libsbml.AST_ARRAYS_FUNCTION_SELECTOR);
+        var ci = new NewASTNode(libsbml.AST_NAME);
+        ci.setName("A");
+        ast.addChild(ci);
+        ci = new NewASTNode(libsbml.AST_NAME);
+        ci.setName("i");
+        ast.addChild(ci);
+        index.setMath(ast);
+
+        speciesRef = reaction.createProduct();
+        speciesRef.setSpecies("C");
+        speciesRef.setConstant(false);
+        refPlugin = (ArraysSpeciesReferencePlugin)speciesRef.getPlugin("arrays");
+        index = refPlugin.createIndex();
+		ast = new NewASTNode(libsbml.AST_ARRAYS_FUNCTION_SELECTOR);
+        ci = new NewASTNode(libsbml.AST_NAME);
+        ci.setName("C");
+        ast.addChild(ci);
+        ci = new NewASTNode(libsbml.AST_NAME);
+        ci.setName("i");
+        ast.addChild(ci);
+        index.setMath(ast);
+
+        libsbml.writeSBMLToFile(doc, "arrays1.xml");
+    }
+}
---
+++ examples/csharp/arrays/CreateArrays2.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using libsbmlcs;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var arraysNs = new ArraysPkgNamespaces();
+        var doc = new SBMLDocument(arraysNs);
+        doc.setPackageRequired("arrays", true);
+        var model = doc.createModel();
+
+        // create parameters
+        var param = model.createParameter();
+        param.setId("n");
+        param.setValue(10);
+        param.setConstant(true);
+
+        param = model.createParameter();
+        param.setId("m");
+        param.setValue(10);
+        param.setConstant(true);
+
+        param = model.createParameter();
+        param.setId("x");
+        param.setValue(5.7);
+        param.setConstant(true);
+
+        var paramPlugin = (ArraysParameterPlugin)param.getPlugin("arrays");
+        var dim = paramPlugin.createDimension();
+        dim.setId("i");
+        dim.setSize("n");
+
+        param = model.createParameter();
+        param.setId("y");
+        param.setConstant(false);
+
+        paramPlugin = (ArraysParameterPlugin)param.getPlugin("arrays");
+        dim = paramPlugin.createDimension();
+        dim.setId("i");
+        dim.setSize("n");
+
+        param = model.createParameter();
+        param.setId("z");
+        param.setConstant(false);
+
+        paramPlugin = (ArraysParameterPlugin)param.getPlugin("arrays");
+        dim = paramPlugin.createDimension();
+        dim.setId("i");
+        dim.setSize("n");
+
+        // create initial assignments
+
+        var assignment = model.createInitialAssignment();
+        assignment.setSymbol("y");
+        var ast = new ASTNode(libsbml.AST_REAL);
+        ast.setValue(3.2);
+        assignment.setMath(ast);
+
+        assignment = model.createInitialAssignment();
+        assignment.setSymbol("z");
+        ast = new ASTNode(libsbml.AST_REAL);
+        ast.setValue(5.7);
+        assignment.setMath(ast);
+
+        var assignmentPlugin = (ArraysInitialAssignmentPlugin)assignment.getPlugin("arrays");
+        dim = assignmentPlugin.createDimension();
+        dim.setId("i");
+        dim.setSize("m");
+
+        var index = assignmentPlugin.createIndex();
+        var newAst = new NewASTNode(libsbml.AST_FUNCTION);
+        newAst.setName("selector");
+        var ci = new NewASTNode(libsbml.AST_NAME);
+        ci.setName("z");
+        newAst.addChild(ci);
+        ci = new NewASTNode(libsbml.AST_NAME);
+        ci.setName("i");
+        newAst.addChild(ci);
+        index.setMath(newAst);
+
+        assignment = model.createInitialAssignment();
+        assignment.setSymbol("z");
+        ast = new ASTNode(libsbml.AST_REAL);
+        ast.setValue(3.2);
+        assignment.setMath(ast);
+
+        assignmentPlugin = (ArraysInitialAssignmentPlugin)assignment.getPlugin("arrays");
+        dim = assignmentPlugin.createDimension();
+        dim.setId("i");
+        dim.setSize("m");
+
+        index = assignmentPlugin.createIndex();
+		newAst = new NewASTNode(libsbml.AST_ARRAYS_FUNCTION_SELECTOR);
+        ci = new NewASTNode(libsbml.AST_NAME);
+        ci.setName("z");
+        newAst.addChild(ci);
+        var plus = new NewASTNode(libsbml.AST_PLUS);
+
+        ci = new NewASTNode(libsbml.AST_NAME);
+        ci.setName("i");
+        plus.addChild(ci);
+        ci = new NewASTNode(libsbml.AST_NAME);
+        ci.setName("m");
+        plus.addChild(ci);
+        newAst.addChild(plus);
+        index.setMath(newAst);
+
+        libsbml.writeSBMLToFile(doc, "arrays2.xml");
+    }
+}
