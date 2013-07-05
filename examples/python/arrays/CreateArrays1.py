2fe824dcf60f315467a8ed8b7cedc1ce7476f778
skeating <s.keating@ucl.ac.uk>
2013-07-05 13:17:56 +0000

    SVN MOVE examples files

---
+++ examples/python/arrays/CreateArrays1.py
@@ -0,0 +1,102 @@
+﻿from libsbml import *
+
+arraysNs = ArraysPkgNamespaces();
+doc = SBMLDocument(arraysNs);
+doc.setPackageRequired("arrays", True);
+model = doc.createModel();
+
+# create compartment
+comp = model.createCompartment();
+comp.setMetaId("dd");
+comp.setId("s");
+comp.setConstant(True);
+
+# set dimensions
+compPlugin = comp.getPlugin("arrays");
+dim = compPlugin.createDimension();
+dim.setId("i");
+dim.setSize("n");
+
+# create species
+species = model.createSpecies();
+species.setId("A");
+species.setCompartment("s");
+species.setHasOnlySubstanceUnits(False);
+species.setBoundaryCondition(False);
+species.setConstant(False);
+
+splugin = species.getPlugin("arrays");
+dim = splugin.createDimension();
+dim.setId("i");
+dim.setSize("n");
+
+species = model.createSpecies();
+species.setId("B");
+species.setCompartment("s");
+species.setHasOnlySubstanceUnits(False);
+species.setBoundaryCondition(False);
+species.setConstant(False);
+
+splugin = species.getPlugin("arrays");
+dim = splugin.createDimension();
+dim.setId("i");
+dim.setSize("n");
+
+species = model.createSpecies();
+species.setId("C");
+species.setCompartment("s");
+species.setHasOnlySubstanceUnits(False);
+species.setBoundaryCondition(False);
+species.setConstant(False);
+
+splugin = species.getPlugin("arrays");
+dim = splugin.createDimension();
+dim.setId("i");
+dim.setSize("n");
+
+# create parameter
+param = model.createParameter();
+param.setId("n");
+param.setValue(100);
+param.setConstant(True);
+
+# create reaction
+reaction = model.createReaction();
+reaction.setId("reaction1");
+reaction.setReversible(False);
+reaction.setFast(False);
+
+reactionPlugin = reaction.getPlugin("arrays");
+dim = reactionPlugin.createDimension();
+dim.setId("i");
+dim.setSize("n");
+
+speciesRef = reaction.createReactant();
+speciesRef.setSpecies("A");
+speciesRef.setConstant(False);
+refPlugin = speciesRef.getPlugin("arrays");
+index = refPlugin.createIndex();
+ast = NewASTNode(AST_ARRAYS_FUNCTION_SELECTOR);
+ci1 = NewASTNode(AST_NAME);
+ci1.setName("A");
+ast.addChild(ci1);
+ci2 = NewASTNode(AST_NAME);
+ci2.setName("i");
+ast.addChild(ci2);
+index.setMath(ast);
+
+speciesRef = reaction.createProduct();
+speciesRef.setSpecies("C");
+speciesRef.setConstant(False);
+refPlugin = speciesRef.getPlugin("arrays");
+index = refPlugin.createIndex();
+ast = NewASTNode(AST_ARRAYS_FUNCTION_SELECTOR);
+ci1 = NewASTNode(AST_NAME);
+ci1.setName("C");
+ast.addChild(ci1);
+ci2 = NewASTNode(AST_NAME);
+ci2.setName("i");
+ast.addChild(ci2);
+index.setMath(ast);
+
+writeSBMLToFile(doc, "arrays1.xml");
---
+++ examples/python/arrays/CreateArrays2.py
@@ -0,0 +1,103 @@
+﻿from libsbml import *
+
+arraysNs = ArraysPkgNamespaces();
+doc = SBMLDocument(arraysNs);
+doc.setPackageRequired("arrays", True);
+model = doc.createModel();
+
+# create parameters
+param = model.createParameter();
+param.setId("n");
+param.setValue(10);
+param.setConstant(True);
+
+param = model.createParameter();
+param.setId("m");
+param.setValue(10);
+param.setConstant(True);
+
+param = model.createParameter();
+param.setId("x");
+param.setValue(5.7);
+param.setConstant(True);
+
+paramPlugin = param.getPlugin("arrays");
+dim = paramPlugin.createDimension();
+dim.setId("i");
+dim.setSize("n");
+
+param = model.createParameter();
+param.setId("y");
+param.setConstant(False);
+
+paramPlugin = param.getPlugin("arrays");
+dim = paramPlugin.createDimension();
+dim.setId("i");
+dim.setSize("n");
+
+param = model.createParameter();
+param.setId("z");
+param.setConstant(False);
+
+paramPlugin = param.getPlugin("arrays");
+dim = paramPlugin.createDimension();
+dim.setId("i");
+dim.setSize("n");
+
+# create initial assignments
+
+assignment = model.createInitialAssignment();
+assignment.setSymbol("y");
+ast = ASTNode(AST_REAL);
+ast.setValue(3.2);
+assignment.setMath(ast);
+
+assignment = model.createInitialAssignment();
+assignment.setSymbol("z");
+ast = ASTNode(AST_REAL);
+ast.setValue(5.7);
+assignment.setMath(ast);
+
+assignmentPlugin = assignment.getPlugin("arrays");
+dim = assignmentPlugin.createDimension();
+dim.setId("i");
+dim.setSize("m");
+
+index = assignmentPlugin.createIndex();
+newAst = NewASTNode(AST_ARRAYS_FUNCTION_SELECTOR);
+ci1 = NewASTNode(AST_NAME);
+ci1.setName("z");
+newAst.addChild(ci1);
+ci2 = NewASTNode(AST_NAME);
+ci2.setName("i");
+newAst.addChild(ci2);
+index.setMath(newAst);
+
+assignment = model.createInitialAssignment();
+assignment.setSymbol("z");
+ast = ASTNode(AST_REAL);
+ast.setValue(3.2);
+assignment.setMath(ast);
+
+assignmentPlugin = assignment.getPlugin("arrays");
+dim = assignmentPlugin.createDimension();
+dim.setId("i");
+dim.setSize("m");
+
+index = assignmentPlugin.createIndex();
+newAst = NewASTNode(AST_ARRAYS_FUNCTION_SELECTOR);
+ci = NewASTNode(AST_NAME);
+ci.setName("z");
+newAst.addChild(ci);
+plus = NewASTNode(AST_PLUS);
+
+ci1 = NewASTNode(AST_NAME);
+ci1.setName("i");
+plus.addChild(ci1);
+ci2 = NewASTNode(AST_NAME);
+ci2.setName("m");
+plus.addChild(ci2);
+newAst.addChild(plus);
+index.setMath(newAst);
+
+writeSBMLToFile(doc, "arrays2.xml");
