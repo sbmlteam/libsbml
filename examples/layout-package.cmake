d53bd9a085d61c5f652248db4a5fe497edf77118
skeating <s.keating@ucl.ac.uk>
2013-10-14 10:22:16 +0000

    added necessary CMake files for new location of layout examples

--- examples/layout-package.cmake
+++ examples/layout-package.cmake
@@ -8,15 +8,15 @@
 #
 
 if (ENABLE_LAYOUT)
-        add_subdirectory(layout)
+#        add_subdirectory(layout)
 #        add_subdirectory(c/layout)
-#        add_subdirectory(c++/layout)
+        add_subdirectory(c++/layout)
 
-#        if(WITH_CSHARP)
-#          add_subdirectory(csharp/layout)
-#        endif(WITH_CSHARP)
+        if(WITH_CSHARP)
+          add_subdirectory(csharp/layout)
+        endif(WITH_CSHARP)
 
-#        if(WITH_JAVA)
-#          add_subdirectory(java/layout)
-#        endif(WITH_JAVA)
+        if(WITH_JAVA)
+          add_subdirectory(java/layout)
+        endif(WITH_JAVA)
 endif(ENABLE_LAYOUT)
