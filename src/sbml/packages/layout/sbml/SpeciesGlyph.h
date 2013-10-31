52117f68341fdd21142dc05d7b33b69cc0aad772
mhucka <mhucka@caltech.edu>
2013-10-31 17:18:19 +0000

    Updated the copyright statements.  The distribution terms originally written in the files are the same LGPL terms we use for all of libSBML, so there didn't seem to be a point in repeating them.  So, I used a modified version of our usual libSBML copyright & distribution terms block, updated to use a date range for EML's contributions from 2004-2008.

--- src/sbml/packages/layout/sbml/SpeciesGlyph.h
+++ src/sbml/packages/layout/sbml/SpeciesGlyph.h
@@ -4,58 +4,30 @@
  * @author  Ralph Gauges
  * 
  * <!--------------------------------------------------------------------------
- * Description : SBML Layout SpeciesGlyph C++ Header
- * Organization: European Media Laboratories Research gGmbH
- * Created     : 2004-07-15
+ * This file is part of libSBML.  Please visit http://sbml.org for more
+ * information about SBML, and the latest version of libSBML.
  *
- * Copyright 2004 European Media Laboratories Research gGmbH
- *
+ * Copyright (C) 2009-2013 jointly by the following organizations: 
+ *     1. California Institute of Technology, Pasadena, CA, USA
+ *     2. EMBL European Bioinformatics Institute (EBML-EBI), Hinxton, UK
+ *  
+ * Copyright (C) 2004-2008 by European Media Laboratories Research gGmbH,
+ *     Heidelberg, Germany
+ * 
  * This library is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published
- * by the Free Software Foundation; either version 2.1 of the License, or
- * any later version.
- *
- * This library is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY, WITHOUT EVEN THE IMPLIED WARRANTY OF
- * MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.  The software and
- * documentation provided hereunder is on an "as is" basis, and the
- * European Media Laboratories Research gGmbH have no obligations to
- * provide maintenance, support, updates, enhancements or modifications.
- * In no event shall the European Media Laboratories Research gGmbH be
- * liable to any party for direct, indirect, special, incidental or
- * consequential damages, including lost profits, arising out of the use of
- * this software and its documentation, even if the European Media
- * Laboratories Research gGmbH have been advised of the possibility of such
- * damage.  See the GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this library; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- *
- * The original code contained here was initially developed by:
- *
- *     Ralph Gauges
- *     Bioinformatics Group
- *     European Media Laboratories Research gGmbH
- *     Schloss-Wolfsbrunnenweg 31c
- *     69118 Heidelberg
- *     Germany
- *
- *     http://www.eml-research.de/english/Research/BCB/
- *     mailto:ralph.gauges@eml-r.villa-bosch.de
- *
- * Contributor(s):
- *
- *     Akiya Jouraku <jouraku@bio.keio.ac.jp>
- *     Modified this file for package extension in libSBML5
- *------------------------------------------------------------------------- -->
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation.  A copy of the license agreement is provided
+ * in the file named "LICENSE.txt" included with this software distribution
+ * and also available online as http://sbml.org/software/libsbml/license.html
+ * ------------------------------------------------------------------------ -->
  *
  * @class SpeciesGlyph
  * @sbmlpackage layout
  * @brief @htmlinclude pkg-marker-layout.html
  * The %SpeciesGlyph represents a Species in the &ldquo;layout&rdquo; package.
  *
- * In addition to the attributes it inherits from GraphicalObject, the SpeciesGlyph object has an optional 'species' attribute.
+ * In addition to the attributes it inherits from GraphicalObject, the
+ * SpeciesGlyph object has an optional 'species' attribute.
  */
 
 #ifndef SpeciesGlyph_H__
--- src/sbml/packages/layout/util/LayoutAnnotation.h
+++ src/sbml/packages/layout/util/LayoutAnnotation.h
@@ -11,19 +11,15 @@
  *     1. California Institute of Technology, Pasadena, CA, USA
  *     2. EMBL European Bioinformatics Institute (EBML-EBI), Hinxton, UK
  *  
- * Copyright (C) 2006-2008 by the California Institute of Technology,
- *     Pasadena, CA, USA 
- *  
- * Copyright (C) 2002-2005 jointly by the following organizations: 
- *     1. California Institute of Technology, Pasadena, CA, USA
- *     2. Japan Science and Technology Agency, Japan
+ * Copyright (C) 2004-2008 by European Media Laboratories Research gGmbH,
+ *     Heidelberg, Germany
  * 
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation.  A copy of the license agreement is provided
  * in the file named "LICENSE.txt" included with this software distribution
  * and also available online as http://sbml.org/software/libsbml/license.html
- * ---------------------------------------------------------------------- -->
+ * ------------------------------------------------------------------------ -->
  */
 
 #ifndef LayoutAnnotation_h
