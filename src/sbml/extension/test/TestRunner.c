9b01185db81618fc2c0460395a36a16a696dc923
fbergmann <fbergman@caltech.edu>
2013-11-05 08:55:59 +0000

    - add test directory

--- src/sbml/extension/test/TestRunner.c
+++ src/sbml/extension/test/TestRunner.c
@@ -55,6 +55,41 @@ Suite *create_suite_TestUnknownPackages      (void);
 END_C_DECLS
 
 
+/**
+ * Global.
+ *
+ * Declared extern in TestReadFromFileN suites.
+ */
+char *TestDataDirectory;
+
+
+/**
+ * Sets TestDataDirectory for the the TestReadFromFileN suites.
+ *
+ * For Automake's distcheck target to work properly, TestDataDirectory must
+ * begin with the value of the environment variable SRCDIR.
+ */
+void
+setTestDataDirectory (void)
+{
+  char *srcdir = getenv("srcdir");
+  int  length  = (srcdir == NULL) ? 0 : strlen(srcdir);
+
+
+  /**
+   * strlen("/test-data/") = 11 + 1 (for NULL) = 12
+   */
+  TestDataDirectory = (char *) safe_calloc( length + 12, sizeof(char) );
+
+  if (srcdir != NULL)
+  {
+    strcpy(TestDataDirectory, srcdir);
+    strcat(TestDataDirectory, "/");
+  }
+
+  strcat(TestDataDirectory, "test-data/");
+}
+
 
 int
 main (void) 
@@ -62,6 +97,8 @@ main (void)
   int num_failed;
   SRunner *runner;
 
+  setTestDataDirectory();
+  
   runner = srunner_create( create_suite_SBaseExtensionPoint() );
   srunner_add_suite( runner, create_suite_SBasePlugin () );
   srunner_add_suite( runner, create_suite_SBasePluginCreator () );
