f368d2c617e0f3bf5ccc2f8edb2dbae0157195a7
skeating <s.keating@ucl.ac.uk>
2014-01-03 17:07:16 +0000

    caught bug where the writeMathML function did not pass the sbml namespaces the new ast branch caught this because it does not assume the l3v1 ns for units

--- src/sbml/Priority.cpp
+++ src/sbml/Priority.cpp
@@ -471,7 +471,8 @@ Priority::writeElements (XMLOutputStream& stream) const
 {
   SBase::writeElements(stream);
 
-  if ( getLevel() > 2 && isSetMath() ) writeMathML(getMath(), stream);
+  if ( getLevel() > 2 && isSetMath() ) writeMathML(getMath(), stream,
+                                                   getSBMLNamespaces());
 
   //
   // (EXTENSION)
