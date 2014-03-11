1a47511b31e19898cc6c3ca06cf9a54572486bb3
mhucka <mhucka@caltech.edu>
2014-03-11 20:05:05 +0000

    1. Updated copyright statements.  (Note: I used a starting date of 2009 because it looks like the actual code did not get created until then, and past copyright statements in things like Makefiles with prior dates were probably copy-pasted.  It probably makes more sense to reach back only as far as the code was first created.)
    2. Removed dos line endings from files that had it.
    
    3. Added svn property eol-style native to those files that were missing it.

--- src/sbml/packages/fbc/extension/FbcExtension.cpp
+++ src/sbml/packages/fbc/extension/FbcExtension.cpp
@@ -7,9 +7,16 @@
  *<!---------------------------------------------------------------------------
  * This file is part of libSBML.  Please visit http://sbml.org for more
  * information about SBML, and the latest version of libSBML.
- *
- * Copyright 2009-2013 California Institute of Technology.
  * 
+ * Copyright (C) 2013-2014 jointly by the following organizations:
+ *     1. California Institute of Technology, Pasadena, CA, USA
+ *     2. EMBL European Bioinformatics Institute (EBML-EBI), Hinxton, UK
+ *     3. University of Heidelberg, Heidelberg, Germany
+ * 
+ * Copyright (C) 2009-2013 jointly by the following organizations: 
+ *     1. California Institute of Technology, Pasadena, CA, USA
+ *     2. EMBL European Bioinformatics Institute (EBML-EBI), Hinxton, UK
+ *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation.  A copy of the license agreement is provided
