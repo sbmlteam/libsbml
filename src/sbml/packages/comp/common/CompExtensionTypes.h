42e44e73883c0bf8e812a3edb5d02da5332db035
mhucka <mhucka@caltech.edu>
2014-03-11 20:15:45 +0000

    1. Updated copyright statements.  I tried to use year ranges that seemed to correspond to the period when comp was actually worked on, and in cases where some files had prior dates (like Makefiles), I removed prior dates because I suspect they were copy-pasted and never correct in the first place.
    2. Removed dos line endings from files that had it.
    
    3. Added svn property eol-style native to those files that were missing it.

--- src/sbml/packages/comp/common/CompExtensionTypes.h
+++ src/sbml/packages/comp/common/CompExtensionTypes.h
@@ -7,10 +7,15 @@
  * This file is part of libSBML.  Please visit http://sbml.org for more
  * information about SBML, and the latest version of libSBML.
  *
- * Copyright 2005-2011 California Institute of Technology.
- * Copyright 2002-2005 California Institute of Technology and
- *                     Japan Science and Technology Corporation.
+ * Copyright (C) 2013-2014 jointly by the following organizations:
+ *     1. California Institute of Technology, Pasadena, CA, USA
+ *     2. EMBL European Bioinformatics Institute (EBML-EBI), Hinxton, UK
+ *     3. University of Heidelberg, Heidelberg, Germany
  * 
+ * Copyright 2011-2012 jointly by the following organizations:
+ *     1. California Institute of Technology, Pasadena, CA, USA
+ *     2. EMBL European Bioinformatics Institute (EBML-EBI), Hinxton, UK
+ *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation.  A copy of the license agreement is provided
