20978db25be8366cfbf17079f8ceb5f0be1a4f76
mhucka <mhucka@caltech.edu>
2014-03-19 17:27:12 +0000

    Fixed typo: EBML-EBI -> EMBL-EBI.

--- examples/python/distrib/createNormalExample.py
+++ examples/python/distrib/createNormalExample.py
@@ -10,7 +10,7 @@
 #
 # Copyright (C) 2009-2013 jointly by the following organizations:
 #     1. California Institute of Technology, Pasadena, CA, USA
-#     2. EMBL European Bioinformatics Institute (EBML-EBI), Hinxton, UK
+#     2. EMBL European Bioinformatics Institute (EMBL-EBI), Hinxton, UK
 #
 # Copyright (C) 2006-2008 by the California Institute of Technology,
 #     Pasadena, CA, USA 
