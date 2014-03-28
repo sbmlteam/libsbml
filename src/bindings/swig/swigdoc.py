0f5c30d58eddc227a3fd49ddcab296efdd208a1d
fbergmann <fbergman@caltech.edu>
2014-03-28 12:24:31 +0000

    - directly include the functions from libsbmlutils for now to fix the broken language binding builds with cmake.

--- src/bindings/swig/swigdoc.py
+++ src/bindings/swig/swigdoc.py
@@ -36,8 +36,8 @@
 #----------------------------------------------------------------------- -->*/
 
 import sys, string, os.path, re
-sys.path.append('../../../docs/src/utilities')
-import argparse, libsbmlutils
+#sys.path.append('../../../docs/src/utilities')
+import argparse#, libsbmlutils
 
 
 #
@@ -138,6 +138,84 @@ allclassdocs = {}
 #
 
 preprocessor_defines = ['SWIG', '__cplusplus']
+
+#
+# contents from libsbmlutils.py
+#
+
+
+#
+# find_classes(X)
+#
+
+def find_classes(arg, swig_too = False):
+    """List class declarations found in .h files.
+
+    ARG can be a single directory or a list of files.  Returns a list of all
+    classes found in all .h or .i files in ARG (either one if present), or if
+    ARG is a directory, found recursively in .h (optionally, .i) files in ARG
+    and its subdirectories.  It does this by searching for Doxygen @class
+    declarations in the comments of the files and %template declarations in
+    .i files.  Skips classes whose names begin with 'doc_'.  The argument
+    swig_too is only necessary if ARG is a directory; if ARG is a list of
+    files, then the intention is clear based on whether the list contains .h
+    files or .i files.
+    """
+
+    if type(arg) is list:
+        return [c for file in arg for c in classes_in_file(file)]
+    else:
+        return classes_in_dir(arg, swig_too)
+
+
+def classes_in_dir(dir, swig_too):
+    files = []
+    for root, dir, found_files in os.walk(dir):
+        for tail in found_files:
+            files.append(os.path.join(root, tail))
+            files = [f for f in files
+                     if (f.endswith('.h') or (swig_too and f.endswith('.i')))]
+            files = filter(lambda x: not x.endswith('fwd.h'), files)
+            files = filter(lambda x: not x.endswith('ExtensionTypes.h'), files)
+    return [c for file in files for c in classes_in_file(file)]
+
+
+def classes_in_file(filename):
+    stream = open(filename)
+    classes = []
+    if filename.endswith('.h'):
+        classes = classes_in_header_file(stream)
+    elif filename.endswith('.i'):
+        classes = classes_in_swig_file(stream)
+    stream.close()
+    return classes
+
+
+def classes_in_header_file(stream):
+    classes = []
+    for line in stream.readlines():
+        start = line.find('@class')
+        if start < 0:
+            continue
+        # Sometimes we have "@class Name." instead of "@class Name"
+        name = re.sub(r'\.', '', line[start + 6:]).strip()
+        # Ignore a few cases.
+        if name and (not (name.startswith("doc_") or name.startswith("is "))):
+            classes.append(name)
+    return classes
+
+
+def classes_in_swig_file(stream):
+    classes = []
+    for line in stream.readlines():
+        start = line.find('%template(')
+        if start < 0:
+            continue
+        end = line.find(')')
+        name = line[start + 10:end].strip()
+        if name:
+          classes.append(name)
+    return classes
 
 
 #
@@ -1571,6 +1649,8 @@ def main (args):
   header_files     = get_header_files(swig_files, h_include_path)
   libsbml_classes  = libsbmlutils.find_classes(header_files)
   libsbml_classes += libsbmlutils.find_classes(swig_files)
+  #libsbml_classes  = libsbmlutils.find_classes(header_files)
+  #libsbml_classes += libsbmlutils.find_classes(swig_files)
 
   try:
     libsbml_classes  = sorted(list(set(libsbml_classes)))
