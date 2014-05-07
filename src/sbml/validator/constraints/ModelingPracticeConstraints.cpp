7ac3951f848690c23ae1734f8b0d17a192da3111
skeating <s.keating@ucl.ac.uk>
2014-05-07 13:09:05 +0000

    Fix for https://www.pivotaltracker.com/story/show/68910590 if a compartment/species has no initial value but there is an initialAssignment or assignmentRule for the element the warning about no initial value is no longer logged

--- src/sbml/validator/constraints/ModelingPracticeConstraints.cpp
+++ src/sbml/validator/constraints/ModelingPracticeConstraints.cpp
@@ -55,15 +55,55 @@ START_CONSTRAINT (80501, Compartment, c)
   //msg =
   //  "It is recommended that the size of a compartment is set.";
 
-  inv( c.isSetSize() == true );
+  bool fail = true;
+
+  // is size set on the element
+  if (c.isSetSize() == true)
+  {
+    fail = false;
+  }
+  else
+  {
+    pre (c.isSetId() == true);
+    // is there an initial assignment/assignment rule that would set the value
+    if (m.getInitialAssignmentBySymbol(c.getId()) != NULL)
+    {
+      fail = false;
+    }
+    else if (m.getAssignmentRuleByVariable(c.getId()) != NULL)
+    {
+      fail = false;
+    }
+  }
+
+  inv( fail == false);
 }
 END_CONSTRAINT
 
 
 START_CONSTRAINT (80601, Species, s)
 {
-  inv_or (s.isSetInitialAmount() == true);
-  inv_or (s.isSetInitialConcentration() == true);
+  bool fail = true;
+
+  if (s.isSetInitialAmount() == true || s.isSetInitialConcentration() == true)
+  {
+    fail = false;
+  }
+  else
+  {
+    pre (s.isSetId() == true);
+    // is there an initial assignment/assignment rule that would set the value
+    if (m.getInitialAssignmentBySymbol(s.getId()) != NULL)
+    {
+      fail = false;
+    }
+    else if (m.getAssignmentRuleByVariable(s.getId()) != NULL)
+    {
+      fail = false;
+    }
+  }
+
+  inv (fail == false);
 }
 END_CONSTRAINT
 
