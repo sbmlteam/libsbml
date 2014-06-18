d8b996e4d3485558dcb9ff2500baaf5e3fe4966d
skeating <s.keating@ucl.ac.uk>
2014-06-18 08:16:57 +0000

    updated copyright info

--- src/sbml/packages/arrays/math/ASTArraysVectorFunctionNode.h
+++ src/sbml/packages/arrays/math/ASTArraysVectorFunctionNode.h
@@ -7,10 +7,15 @@
  * This file is part of libSBML.  Please visit http://sbml.org for more
  * information about SBML, and the latest version of libSBML.
  *
- * Copyright (C) 2009-2012 jointly by the following organizations: 
+ * Copyright (C) 2013-2014 jointly by the following organizations:
  *     1. California Institute of Technology, Pasadena, CA, USA
  *     2. EMBL European Bioinformatics Institute (EMBL-EBI), Hinxton, UK
- *  
+ *     3. Unversity of Heidelberg, Heidelberg, Germany
+ *
+ * Copyright (C) 2009-2013 jointly by the following organizations:
+ *     1. California Institute of Technology, Pasadena, CA, USA
+ *     2. EMBL European Bioinformatics Institute (EMBL-EBI), Hinxton, UK
+ *
  * Copyright (C) 2006-2008 by the California Institute of Technology,
  *     Pasadena, CA, USA 
  *  
