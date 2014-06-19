0f96d853d9dc22588fdaeea792f6012435073866
skeating <s.keating@ucl.ac.uk>
2014-06-19 08:36:01 +0000

    the moral is: don't auto generate a typo

--- src/sbml/packages/arrays/validator/ArraysIdentifierConsistencyValidator.h
+++ src/sbml/packages/arrays/validator/ArraysIdentifierConsistencyValidator.h
@@ -12,7 +12,7 @@
  * Copyright (C) 2013-2014 jointly by the following organizations:
  *     1. California Institute of Technology, Pasadena, CA, USA
  *     2. EMBL European Bioinformatics Institute (EMBL-EBI), Hinxton, UK
- *     3. Unversity of Heidelberg, Heidelberg, Germany
+ *     3. University of Heidelberg, Heidelberg, Germany
  *
  * Copyright (C) 2009-2013 jointly by the following organizations:
  *     1. California Institute of Technology, Pasadena, CA, USA
