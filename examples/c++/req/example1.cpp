4efec38601fd50d82f1458880c6680bdd64554e2
skeating <s.keating@ucl.ac.uk>
2014-06-23 11:59:25 +0000

    got the data types wrong - but they are right now rewrote the req example

--- examples/c++/req/example1.cpp
+++ examples/c++/req/example1.cpp
@@ -11,43 +11,48 @@
  */
 
 #include <sbml/SBMLTypes.h>
-#include <sbml/packages/req/common/RequiredElementsExtensionTypes.h>
+#include <sbml/packages/req/common/ReqExtensionTypes.h>
 
 #include <sbml/extension/SBMLExtensionRegister.h>
 #include <sbml/extension/SBMLExtensionRegistry.h>
 
 LIBSBML_CPP_NAMESPACE_USE
 
-static SBMLExtensionRegister<RequiredElementsExtension> requiredElementsExtensionRegistry;
+static SBMLExtensionRegister<ReqExtension> requiredElementsExtensionRegistry;
 
-int main(int argc,char** argv){
+int main(int argc,char** argv)
+{
+  SBMLNamespaces sbmlns(3,1,"req",1);
 
+  // create the document
 
-SBMLNamespaces sbmlns(3,1,"req",1);
+  SBMLDocument *document = new SBMLDocument(&sbmlns);
 
-// create the document
+  SBMLDocumentPlugin* dplugin;
+  dplugin = static_cast<SBMLDocumentPlugin*>(document->getPlugin("req"));
+  dplugin->setRequired(true);
 
-SBMLDocument *document = new SBMLDocument(&sbmlns);
+  // create the Model
 
-// create the Model
+  Model* model=document->createModel();
 
-Model* model=document->createModel();
+  // create a parameter
 
-// create a parameter
+  Parameter* parameter = model->createParameter();
+  parameter->setId("x");
+  parameter->setConstant(true);
 
-Parameter* parameter = model->createParameter();
-parameter->setId("x");
-parameter->setConstant(true);
+  // get the required plugin
+  ReqSBasePlugin* splugin = static_cast<ReqSBasePlugin*>(parameter->getPlugin("req"));
 
-// get the required plugin
-RequiredElementsSBasePlugin* splugin = static_cast<RequiredElementsSBasePlugin*>(parameter->getPlugin("req"));
+  // tell the plugin that the math is overridden
+  ChangedMath* changed = splugin->createChangedMath ();
 
-// tell the plugin that the math is overridden
-splugin->setMathOverridden("spatial");
-splugin->setCoreHasAlternateMath(true);
+  changed->setChangedBy("http://www.sbml.org/sbml/level3/version1/spatial/version1");
+  changed->setViableWithoutChange(false);
 
-writeSBML(document,"req_example1.xml");
-delete document;
+  writeSBML(document,"req_example1.xml");
+  delete document;
 
 }
 
