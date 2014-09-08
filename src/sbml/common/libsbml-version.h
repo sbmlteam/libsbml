71a8fa39179a714793237a299a25473b26fed811
skeating <s.keating@ucl.ac.uk>
2014-09-08 07:49:58 +0000

    when I changed the version number I failed to update all teh files that use it

--- src/sbml/common/libsbml-version.h
+++ src/sbml/common/libsbml-version.h
@@ -42,7 +42,7 @@
  *
  * A version string of the form "1.2.3".
  */
-#define LIBSBML_DOTTED_VERSION	"5.10.2"
+#define LIBSBML_DOTTED_VERSION	"5.10.3"
 
 
 /**
@@ -53,7 +53,7 @@
  * libSBML is released, making it easy to use less-than and greater-than
  * comparisons when testing versions numbers.
  */
-#define LIBSBML_VERSION		51002
+#define LIBSBML_VERSION		51003
 
 
 /**
@@ -61,7 +61,7 @@
  *
  * The numeric version as a string: version 1.2.3 becomes "10203".
  */
-#define LIBSBML_VERSION_STRING	"51002"
+#define LIBSBML_VERSION_STRING	"51003"
 
 
 LIBSBML_CPP_NAMESPACE_BEGIN
