1c085a78a59fc6fef2100719be12c49fce180c2d
mhucka <mhucka@caltech.edu>
2014-10-11 03:09:10 +0000

    Fixed messed up indentation.

--- docs/src/utilities/generate-class-name-list.py
+++ docs/src/utilities/generate-class-name-list.py
@@ -49,14 +49,13 @@ def main(args):
     if len(args) != 2:
         print ("Must be given one argument: the path to the libSBML src/sbml dir")
         sys.exit(1)
-
-  try:
-      for c in sorted(set(find_classes(args[1], True))):
-          print (c)
-      except (NameError,):
-          classes.sort()
-          for c in classes:
-              print (c)
+    try:
+        for c in sorted(set(find_classes(args[1], True))):
+            print (c)
+        except (NameError,):
+            classes.sort()
+            for c in classes:
+                print (c)
 
 if __name__ == '__main__':
     main(sys.argv)
