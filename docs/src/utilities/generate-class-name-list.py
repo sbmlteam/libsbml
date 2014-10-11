a9fda0dfc012340196d18ffb2a6444aab29e1cd2
mhucka <mhucka@caltech.edu>
2014-10-11 02:53:16 +0000

    Made indentation more consistent.

--- docs/src/utilities/generate-class-name-list.py
+++ docs/src/utilities/generate-class-name-list.py
@@ -46,17 +46,17 @@ from libsbmlutils import find_classes
 
 
 def main(args):
-  if len(args) != 2:
-      print ("Must be given one argument: the path to the libSBML src/sbml dir")
-      sys.exit(1)
+    if len(args) != 2:
+        print ("Must be given one argument: the path to the libSBML src/sbml dir")
+        sys.exit(1)
 
   try:
-    for c in sorted(set(find_classes(args[1], True))):
-        print (c)
-  except (NameError,):
-    classes.sort()
-    for c in classes:
-        print (c)
+      for c in sorted(set(find_classes(args[1], True))):
+          print (c)
+      except (NameError,):
+          classes.sort()
+          for c in classes:
+              print (c)
 
 if __name__ == '__main__':
-  main(sys.argv)
+    main(sys.argv)
