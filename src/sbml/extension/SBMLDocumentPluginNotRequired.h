d0ca5f73064ee7a7c55cdad58c4b79a68c903688
mhucka <mhucka@caltech.edu>
2014-11-17 00:50:11 +0000

    Added documentation.

--- src/sbml/extension/SBMLDocumentPluginNotRequired.h
+++ src/sbml/extension/SBMLDocumentPluginNotRequired.h
@@ -1,10 +1,9 @@
 /**
  * @file    SBMLDocumentPluginNotRequired.h
- * @brief   Definition of SBMLDocumentPluginNotRequired, the plugin class of 
+ * @brief   Definition of SBMLDocumentPluginNotRequired, the plugin class of
  *          layout package for the SBMLDocument element, whose 'required' attribute
  *          must be 'false'.
  * @author  Lucian Smith
- *
  *
  * <!--------------------------------------------------------------------------
  * This file is part of libSBML.  Please visit http://sbml.org for more
@@ -15,17 +14,17 @@
  *     2. EMBL European Bioinformatics Institute (EMBL-EBI), Hinxton, UK
  *     3. University of Heidelberg, Heidelberg, Germany
  *
- * Copyright (C) 2009-2013 jointly by the following organizations: 
+ * Copyright (C) 2009-2013 jointly by the following organizations:
  *     1. California Institute of Technology, Pasadena, CA, USA
  *     2. EMBL European Bioinformatics Institute (EMBL-EBI), Hinxton, UK
- *  
+ *
  * Copyright (C) 2006-2008 by the California Institute of Technology,
- *     Pasadena, CA, USA 
- *  
- * Copyright (C) 2002-2005 jointly by the following organizations: 
+ *     Pasadena, CA, USA
+ *
+ * Copyright (C) 2002-2005 jointly by the following organizations:
  *     1. California Institute of Technology, Pasadena, CA, USA
  *     2. Japan Science and Technology Agency, Japan
- * 
+ *
  * This library is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation.  A copy of the license agreement is provided
@@ -36,12 +35,18 @@
  * @class SBMLDocumentPluginNotRequired
  * @sbmlbrief{core} Base class for non-required Level 3 packages plug-ins.
  *
- * The SBMLDocumentPluginNotRequired class extends the SBMLDocumentPlugin class, and
- * will add a validation error to a read-in SBML Document that has the package's 
- * 'required' flag set to @c true:  for all packages, the value of the 'required' flag
- * is set by the specification itself to be @c true or @c false, depending on whether
- * constructs in the package can potentially be used to change the mathematics of
- * the model.
+ * The SBMLDocumentPluginNotRequired class extends the SBMLDocumentPlugin
+ * class, adding a validation test such that it reports an error if an SBML
+ * document read in has a "required" flag set to @c true.  The purpose of
+ * this is to make it simpler to implement packages that are supposed to have
+ * "required" flag values of @c false (such as %Layout and other SBML
+ * packages that cannot affect the mathematical interpretation of a model).
+ *
+ * @note This class is not used currently in any package extension.  This
+ * class was introduced at a time when libSBML did not provide the necessary
+ * hooks into the validation system for packages to add their own validation
+ * rules, but this has since changed.  New developments should use the
+ * regular SBMLDocumentPlugin class instead of this one.
  */
 
 #ifndef SBMLDocumentPluginNotRequired_h
@@ -58,20 +63,38 @@ class LIBSBML_EXTERN SBMLDocumentPluginNotRequired : public SBMLDocumentPlugin
 public:
 
   /**
-   * Constructor
+   * Creates a new SBMLDocumentPluginNotRequired object using the given parameters.
+   *
+   * @copydetails doc_what_are_xmlnamespaces
+   *
+   * @copydetails doc_what_are_sbmlnamespaces
+   *
+   * @param uri the URI of the SBML Level&nbsp;3 package implemented by
+   * this libSBML package extension.
+   *
+   * @param prefix the XML namespace prefix being used for the package.
+   *
+   * @param sbmlns the SBMLNamespaces object for the package.
    */
   SBMLDocumentPluginNotRequired (const std::string &uri, const std::string &prefix,
                                  SBMLNamespaces *sbmlns);
 
 
   /**
-   * Copy constructor. Creates a copy of this SBase object.
+   * Copy constructor.
+   *
+   * This creates a copy of this object.
+   *
+   * @param orig the SBMLDocumentPluginNotRequired instance to copy.
    */
   SBMLDocumentPluginNotRequired(const SBMLDocumentPluginNotRequired& orig);
 
 
   /**
-   * Assignment operator for SBMLDocumentPlugin.
+   * Assignment operator for SBMLDocumentPluginNotRequired.
+   *
+   * @param orig The object whose values are used as the basis of the
+   * assignment.
    */
   SBMLDocumentPluginNotRequired& operator=(const SBMLDocumentPluginNotRequired& orig);
 
@@ -80,6 +103,7 @@ public:
    * Destroy this object.
    */
   virtual ~SBMLDocumentPluginNotRequired ();
+
 
 #ifndef SWIG
 
@@ -103,7 +127,7 @@ public:
    * in SBMLDocument element.  This package is required to set this value
    * to 'false'; attempting to set it to 'true' will result in an error.
    *
-   * @param value the bool value of "required" attribute of corresponding 
+   * @param value the bool value of "required" attribute of corresponding
    * package in SBMLDocument element.
    *
    * @copydetails doc_returns_success_code
