9c04065c2b97e0916bb9e4c4c0495149fead241c
skeating <s.keating@ucl.ac.uk>
2015-01-11 11:18:56 +0000

    updating version number for release

--- src/sbml/common/libsbml-version.h
+++ src/sbml/common/libsbml-version.h
@@ -42,7 +42,7 @@
  *
  * A version string of the form "1.2.3".
  */
-#define LIBSBML_DOTTED_VERSION	"5.11.1"
+#define LIBSBML_DOTTED_VERSION	"5.11.2"
 
 
 /**
@@ -53,7 +53,7 @@
  * libSBML is released, making it easy to use less-than and greater-than
  * comparisons when testing versions numbers.
  */
-#define LIBSBML_VERSION		51101
+#define LIBSBML_VERSION		51102
 
 
 /**
@@ -61,7 +61,7 @@
  *
  * The numeric version as a string: version 1.2.3 becomes "10203".
  */
-#define LIBSBML_VERSION_STRING	"51101"
+#define LIBSBML_VERSION_STRING	"51102"
 
 
 LIBSBML_CPP_NAMESPACE_BEGIN
