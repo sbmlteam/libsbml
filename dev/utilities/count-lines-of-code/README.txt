b84eadf11b6d02fad6bb57dcbf8fe71d958afcc0
skeating <s.keating@ucl.ac.uk>
2015-06-22 08:14:53 +0000

    added the scripts used to cloc

---
+++ dev/utilities/count-lines-of-code/README.txt
@@ -0,0 +1,10 @@
+Invokes the cloc program to count lines of code in the libsbml directory.
+
+WINDOWS
+=======
+
+Two batch files:
+
+src-no-bindings: produces no-bindings.csv which counts the loc excluding the bindings
+
+bindings-onle: produces bindings.csv which counts loc in the bindings directory only
---
+++ dev/utilities/count-lines-of-code/bindings-only.bat
@@ -0,0 +1,29 @@
+@ echo off
+REM 
+REM This script counts lines of code.
+REM 
+
+SET THIS_DIR=%~dp0
+SET CLOC=%THIS_DIR%\bin\cloc-1.62 --autoconf
+SET LIBSBML_DIR=%~dp0\..\..\..
+SET EXCLUDE_DIRS=dev,docs,examples,macosx
+
+REM additional things to do
+REM make .t files counted as Perl (these are Perl test files)
+SET FORCED="Perl",t
+
+REM need to set these as appropriate for the count wanted
+SET ADDITIONAL_EXCLUDES=
+SET OUTPUT_FILE=%THIS_DIR%\bindings.csv
+
+REM run the count
+%CLOC% %LIBSBML_DIR%\src\bindings --exclude-dir=%EXCLUDE_DIRS%,%ADDITIONAL_EXCLUDES% --report-file=%OUTPUT_FILE% --csv --force-lang=%FORCED%
+
+REM unset variables
+SET THIS_DIR=
+SET CLOC=
+SET LIBSBML_DIR=
+SET EXCLUDE_DIRS=
+SET FORCED=
+SET ADDITIONAL_EXCLUDES=
+SET OUTPUT_FILE=
