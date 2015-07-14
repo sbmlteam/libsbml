736366a379b0bfa6437d348ef2c8b33f4aff491d
fbergmann <fbergman@caltech.edu>
2015-07-14 13:51:36 +0000

    - fix duplicated symbol

--- src/sbml/packages/fbc/sbml/Association.cpp
+++ src/sbml/packages/fbc/sbml/Association.cpp
@@ -34,6 +34,8 @@
 #include <sbml/xml/XMLInputStream.h>
 #include <sbml/xml/XMLOutputStream.h>
 
+#include <sbml/util/util.h>
+
 #include <sbml/packages/fbc/sbml/Association.h>
 #include <sbml/packages/fbc/extension/FbcExtension.h>
 #include <sbml/math/FormulaParser.h>
@@ -242,16 +244,6 @@ Association::unsetReference ()
     return LIBSBML_OPERATION_SUCCESS;
   return LIBSBML_OPERATION_FAILED;
 
-}
-
-void replaceAllSubStrings(std::string& str, const std::string& from, const std::string& to) {
-  if(from.empty())
-    return;
-  size_t start_pos = 0;
-  while((start_pos = str.find(from, start_pos)) != std::string::npos) {
-    str.replace(start_pos, from.length(), to);
-    start_pos += to.length(); // In case 'to' contains 'from', like replacing 'x' with 'yx'
-  }
 }
 
 Association* toAssociation(const ASTNode* node);
