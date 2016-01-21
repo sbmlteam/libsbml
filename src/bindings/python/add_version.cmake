801192a9433142af1cdfb7da0a5d930c4bbd4458
fbergmann <fbergman@caltech.edu>
2016-01-21 14:58:38 +0000

    - issue #112144393: install options to install into prefix and using setup.py

--- src/bindings/python/add_version.cmake
+++ src/bindings/python/add_version.cmake
@@ -36,8 +36,8 @@ message("Python: Add Version to generated file")
 if(NOT EXISTS ${CUR_BIN_DIRECTORY}/libsbml.py)
   message(FATAL_ERROR "  The SWIG wrapper has not yet been created")
 else()
-  file(APPEND ${CUR_BIN_DIRECTORY}/libsbml.py """
+  file(APPEND ${CUR_BIN_DIRECTORY}/libsbml.py "
 global __version__
 __version__ = '${VERSION}'
-  """)
+")
 endif()
