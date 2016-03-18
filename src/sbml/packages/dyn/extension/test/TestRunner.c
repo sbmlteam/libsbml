3c2e3318f9cf14c736b649d94b0f13c30e22d285
skeating <s.keating@ucl.ac.uk>
2016-03-18 10:09:51 +0000

    add vld header to tests

--- src/sbml/packages/dyn/extension/test/TestRunner.c
+++ src/sbml/packages/dyn/extension/test/TestRunner.c
@@ -15,6 +15,10 @@
 #include <sbml/common/extern.h>
 #include <sbml/util/memory.h>
 
+#ifdef LIBSBML_USE_VLD
+  #include <vld.h>
+#endif
+
 #if defined(__cplusplus)
 LIBSBML_CPP_NAMESPACE_USE
 CK_CPPSTART
