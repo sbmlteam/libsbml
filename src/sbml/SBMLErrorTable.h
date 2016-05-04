b06b6af40844b2946d6e46e6313ea2fa4d3d188d
skeating <s.keating@ucl.ac.uk>
2016-05-04 11:05:57 +0000

    fixed the error I missed and made strict units a conversion error

--- src/sbml/SBMLErrorTable.h
+++ src/sbml/SBMLErrorTable.h
@@ -8906,10 +8906,10 @@ static const sbmlErrorTableEntry errorTable[] =
     LIBSBML_SEV_NOT_APPLICABLE,
     LIBSBML_SEV_NOT_APPLICABLE,
     LIBSBML_SEV_NOT_APPLICABLE,
-    LIBSBML_SEV_ERROR,
-    LIBSBML_SEV_ERROR,
-    LIBSBML_SEV_ERROR,
-    LIBSBML_SEV_ERROR,
+    LIBSBML_SEV_WARNING,
+    LIBSBML_SEV_WARNING,
+    LIBSBML_SEV_WARNING,
+    LIBSBML_SEV_WARNING,
     "SBML Level 2 Version 4 removed the requirement that all sboTerms "
     "be drawn from a given branch of SBO (http://www.biomodels.net/SBO/). "
     "This model contains sboTerms that produce "
@@ -9169,10 +9169,10 @@ static const sbmlErrorTableEntry errorTable[] =
     LIBSBML_SEV_NOT_APPLICABLE,
     LIBSBML_SEV_NOT_APPLICABLE,
     LIBSBML_SEV_NOT_APPLICABLE,
-    LIBSBML_SEV_WARNING,
-    LIBSBML_SEV_WARNING,
-    LIBSBML_SEV_WARNING,
-    LIBSBML_SEV_WARNING,
+    LIBSBML_SEV_ERROR,
+    LIBSBML_SEV_ERROR,
+    LIBSBML_SEV_ERROR,
+    LIBSBML_SEV_ERROR,
     "SBML Level 2 Version 4 removed the requirement that all units "
     "be consistent.  This model contains units that produce "
     "inconsistencies and thus conversion to Level 2 Version 3 "
