ad6c293413874220275d12de035f54feb3818326
fbergmann <fbergman@caltech.edu>
2016-08-03 09:00:10 +0000

    - compilation of R bindings on osx fails if string.h is included (as generated by swig) - also ensure that we link against stdc++ instead of c++ - move modification of SWIG generated files into the swig target

--- src/bindings/r/patch-swig-file.cmake
+++ src/bindings/r/patch-swig-file.cmake
@@ -4,11 +4,26 @@ endif()
 
 file(READ "${FILENAME}" SOURCECODE)
 
-string(REPLACE "'get''get'" "'get','get'" SOURCECODE "${SOURCECODE}" )
-string(REPLACE "'get''get'" "'get','get'" SOURCECODE "${SOURCECODE}" )
-string(REPLACE "'get''get'" "'get','get'" SOURCECODE "${SOURCECODE}" )
-string(REPLACE "if ( &&" "if (" SOURCECODE "${SOURCECODE}" )
-string(REPLACE "if ()" "if(TRUE)" SOURCECODE "${SOURCECODE}" )
+if (FILENAME  MATCHES "libSBML.R")
+  
+  
+  string(REPLACE "'get''get'" "'get','get'" SOURCECODE "${SOURCECODE}" )
+  string(REPLACE "'get''get'" "'get','get'" SOURCECODE "${SOURCECODE}" )
+  string(REPLACE "'get''get'" "'get','get'" SOURCECODE "${SOURCECODE}" )
+  string(REPLACE "if ( &&" "if (" SOURCECODE "${SOURCECODE}" )
+  string(REPLACE "if ()" "if(TRUE)" SOURCECODE "${SOURCECODE}" )
+  
+  file(WRITE "${FILENAME}" "${SOURCECODE}")
+  message (STATUS "Patched libSBML.R")
 
-file(WRITE "${FILENAME}" "${SOURCECODE}")
-message (STATUS "Patched libSBML.R")
+endif()
+
+
+if (FILENAME  MATCHES "wrap.cpp")
+
+  string(REPLACE "string.h" "cstring" SOURCECODE "${SOURCECODE}" )
+  
+  file(WRITE "${FILENAME}" "${SOURCECODE}")
+  message (STATUS "Patched libsbml_wrap.cpp")
+
+endif()
