49b85c593798c719135b801e6b456422df890f12
fbergmann <fbergman@caltech.edu>
2016-10-13 14:09:41 +0000

    - specify files to be ignored - added /bigobj to compile flags to allow larger files to be compiled

--- dev/utilities/build-python/build_all.bat
+++ dev/utilities/build-python/build_all.bat
@@ -2,7 +2,21 @@
 SET BASE_DIR=%~dp0
 Setlocal EnableDelayedExpansion
 
+:: build libsbml
 call build_libsbml.bat
+
+:: create all python binaries
 call build_python.bat
+
+:: create conda binaries in dist\conda
 call build_conda.bat 
+
+:: you can limit what will be deleted by providing some 
+:: options here. They are: 
+::
+::          ALL     - delete all files created
+::          BUILD   - delete build dirs
+::          INSTALL - delete install dirs
+::          TEMP    - delete python temp files and dirs
+::
 call remove_files.bat
--- dev/utilities/build-python/build_python.bat
+++ dev/utilities/build-python/build_python.bat
@@ -12,12 +12,23 @@ cmake -DSRC_DIR=%LIBSBML_SRC% -DBUILD_DIR=%BUILD_DIR% -DOUT_DIR=%THIS_DIR%\out -
 
 :: create builds for different python versions
 :: build in parallel (means that installers | wheels have to be created later)
+::
+:: if start is being used, the builds will be carried out in :: parallel. start needs the the argument EXIT_WHEN_DONE, 
+:: otherwise the console window will remain open. 
+::
+:: if call is being used that argument cannot be given 
+:: otherwise the build will stop. 
+:: 
+:: the lines below carry out the 2.5, 2.6 and 2.7 build 
+:: in parallel, as well the 3.1, 3.2 and 3.3 build and the 
+:: 3.4 and 3.5 build. 
+::
 start buildV.bat 2 5 BUILD EXIT_WHEN_DONE
 start buildV.bat 2 6 BUILD EXIT_WHEN_DONE
 call buildV.bat 2 7 BUILD 
 start buildV.bat 3 1 BUILD EXIT_WHEN_DONE
 start buildV.bat 3 2 BUILD EXIT_WHEN_DONE
-start buildV.bat 3 3 BUILD EXIT_WHEN_DONE
+call buildV.bat 3 3 BUILD
 start buildV.bat 3 4 BUILD EXIT_WHEN_DONE
 call buildV.bat 3 5 BUILD
 
