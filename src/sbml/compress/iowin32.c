b09292802dafe0fbf1f8971682a9157a46daac98
fbergmann <fbergman@caltech.edu>
2017-03-16 21:25:05 +0000

    - ensure that iowin32 is not compiled on non-windows systems

--- src/sbml/compress/iowin32.c
+++ src/sbml/compress/iowin32.c
@@ -12,6 +12,7 @@
    See the accompanying LICENSE file for the full text of the license.
 */
 
+#ifdef WIN32
 #include <stdlib.h>
 #include <tchar.h>
 
@@ -583,3 +584,5 @@ void fill_win32_filefunc64W(zlib_filefunc64_def* pzlib_filefunc_def)
     pzlib_filefunc_def->zerror_file = win32_error_file_func;
     pzlib_filefunc_def->opaque = NULL;
 }
+
+#endif // WIN32
