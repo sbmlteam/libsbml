71b668304ed19224d6bc0e0b9bbc9dd76dda1b07
skeating <s.keating@ucl.ac.uk>
2017-03-22 16:44:11 +0000

    removed warnings

--- src/sbml/packages/spatial/extension/SpatialReactionPlugin.cpp
+++ src/sbml/packages/spatial/extension/SpatialReactionPlugin.cpp
@@ -565,7 +565,6 @@ SpatialReactionPlugin::readAttributes(const XMLAttributes& attributes,
   unsigned int version = getVersion();
   unsigned int pkgVersion = getPackageVersion();
   unsigned int numErrs;
-  bool assigned = false;
   SBMLErrorLog* log = getErrorLog();
 
   SBasePlugin::readAttributes(attributes, expectedAttributes);
