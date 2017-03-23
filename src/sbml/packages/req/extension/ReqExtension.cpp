98566e673eeb28cc8bc1f82437aac253c219b67e
skeating <s.keating@ucl.ac.uk>
2017-03-23 10:28:42 +0000

    experimental packages can use l3v1 or l3v2 https://www.pivotaltracker.com/story/show/141844619

--- src/sbml/packages/req/extension/ReqExtension.cpp
+++ src/sbml/packages/req/extension/ReqExtension.cpp
@@ -207,7 +207,7 @@ ReqExtension::getURI(unsigned int sbmlLevel,
 {
   if (sbmlLevel == 3)
   {
-    if (sbmlVersion == 1)
+    if (sbmlVersion == 1 || sbmlVersion == 2)
     {
       if (pkgVersion == 1)
       {
