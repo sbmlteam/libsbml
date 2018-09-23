5eb6bf6f66f4b62722aa07be9c6e5ea2a17dec5a
skeating <s.keating@ucl.ac.uk>
2018-09-23 11:51:56 +0000

    added all downcast files for extended math - although may be unecessary !

---
+++ src/bindings/python/local-downcast-plugins-l3v2extendedmath.cpp
@@ -0,0 +1,11 @@
+#ifdef USE_L3V2EXTENDEDMATH
+if (pkgName == "l3v2extendedmath")
+{
+  if(sb->getTypeCode() == SBML_DOCUMENT)
+  {
+    return SWIGTYPE_p_L3v2extendedmathSBMLDocumentPlugin;
+  }
+}
+
+#endif // USE_FBC 
+
