2466adce2464973aa7ab4b2fd237a6ed6e8234cc
skeating <s.keating@ucl.ac.uk>
2018-12-05 16:20:04 +0000

    update readmes for SF

--- dev/misc-files/readmes-for-experimental-release-downloads/makeSFDir.bat
+++ dev/misc-files/readmes-for-experimental-release-downloads/makeSFDir.bat
@@ -4,7 +4,7 @@ REM This script creates the directory structure for SF
 REM 
 
 SET THIS_DIR=%~dp0
-set VERSION=5.11.6-experimental
+set VERSION=5.17.2-experimental
 SET DIST_DIR=%~dp0\%VERSION%
 SET TEST_DIR=%~dp0\source
 
