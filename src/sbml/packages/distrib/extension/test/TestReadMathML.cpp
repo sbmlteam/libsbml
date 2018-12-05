fa037266563e4fee2ff4eef5a7796f79a0757a8b
skeating <s.keating@ucl.ac.uk>
2018-12-05 13:00:52 +0000

    fix for strict includes

--- src/sbml/packages/distrib/extension/test/TestReadMathML.cpp
+++ src/sbml/packages/distrib/extension/test/TestReadMathML.cpp
@@ -38,6 +38,7 @@
 
 #include <sbml/math/FormulaFormatter.h>
 #include <sbml/math/FormulaParser.h>
+#include <sbml/math/L3FormulaFormatter.h>
 #include <sbml/math/ASTNode.h>
 #include <sbml/math/MathML.h>
 
