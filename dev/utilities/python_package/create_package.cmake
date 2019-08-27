dfa7ed5aacae630b057ac4cbf5396db9b004c2a8
fbergmann <fbergman@caltech.edu>
2019-08-27 13:30:49 +0000

    - issue #168098033: swig is generating 'object' rather than '_object', thus search and replace would not attach python3 metaclass

--- dev/utilities/python_package/create_package.cmake
+++ dev/utilities/python_package/create_package.cmake
@@ -358,6 +358,26 @@ string(REPLACE
   "class SBase(_object, metaclass=AutoProperty):"
   init3_script ${init_script}
 )
+
+string(REPLACE 
+  "class SBase(object):"
+  "class SBase(object, metaclass=AutoProperty):"
+  init3_script ${init_script}
+)
+
+string(REPLACE 
+  "class SBasePlugin(_object):"
+  "class SBasePlugin(_object, metaclass=AutoProperty):"
+  init3_script ${init_script}
+)
+
+string(REPLACE 
+  "class SBasePlugin(object):"
+  "class SBasePlugin(object, metaclass=AutoProperty):"
+  init3_script ${init_script}
+)
+
+
 file(WRITE ${OUT_DIR}/script/libsbml3.py ${init3_script})
 
 
