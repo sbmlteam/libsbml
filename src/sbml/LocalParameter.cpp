69559c26c59d4947d5e9835c9dd53d2f68c0f770
Jamie Quinn <jamiejquinn@jamiejquinn.com>
2020-12-11 15:53:41 +0000

    Removed commented out code from LocalParameter

--- src/sbml/LocalParameter.cpp
+++ src/sbml/LocalParameter.cpp
@@ -383,35 +383,6 @@ LocalParameter::getAttribute(const std::string& attributeName,
 /** @cond doxygenLibsbmlInternal */
 
 /*
- * Returns the value of the "attributeName" attribute of this LocalParameter.
- */
-//int
-//LocalParameter::getAttribute(const std::string& attributeName,
-//                             const char* value) const
-//{
-//  int return_value = SBase::getAttribute(attributeName, value);
-//
-//  if (return_value == LIBSBML_OPERATION_SUCCESS)
-//  {
-//    return return_value;
-//  }
-//
-//  if (attributeName == "units")
-//  {
-//    value = getUnits().c_str();
-//    return_value = LIBSBML_OPERATION_SUCCESS;
-//  }
-//
-//  return return_value;
-//}
-
-/** @endcond */
-
-
-
-/** @cond doxygenLibsbmlInternal */
-
-/*
  * Predicate returning @c true if this LocalParameter's attribute
  * "attributeName" is set.
  */
@@ -528,29 +499,6 @@ LocalParameter::setAttribute(const std::string& attributeName,
 
   return return_value;
 }
-
-/** @endcond */
-
-
-
-/** @cond doxygenLibsbmlInternal */
-
-/*
- * Sets the value of the "attributeName" attribute of this LocalParameter.
- */
-//int
-//LocalParameter::setAttribute(const std::string& attributeName,
-//                             const char* value)
-//{
-//  int return_value = SBase::setAttribute(attributeName, value);
-//
-//  if (attributeName == "units")
-//  {
-//    return_value = setUnits(value);
-//  }
-//
-//  return return_value;
-//}
 
 /** @endcond */
 
