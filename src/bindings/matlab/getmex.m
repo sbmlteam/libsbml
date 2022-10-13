b58daf0b85ef028a2c1930016eb3b947d5f57f86
Sarah <s.keating@ucl.ac.uk>
2022-10-13 13:17:14 +0100

    indents fixed

---
+++ src/bindings/matlab/getmex.m
@@ -0,0 +1,2 @@
+copyfile("C:\Development\SBML\builds\matlab\src\bindings\matlab\Debug\TranslateSBML.mexw64")
+copyfile("C:\Development\SBML\builds\matlab\src\bindings\matlab\Debug\OutputSBML.mexw64")
