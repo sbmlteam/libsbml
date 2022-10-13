9b83c5f2babcca509e2467fd68a433c7f2cfd7f2
Sarah <s.keating@ucl.ac.uk>
2022-10-13 14:45:40 +0100

    shouldnt commit

--- src/bindings/matlab/getmex.m
+++
@@ -1,2 +0,0 @@
-copyfile("C:\Development\SBML\builds\matlab\src\bindings\matlab\Debug\TranslateSBML.mexw64")
-copyfile("C:\Development\SBML\builds\matlab\src\bindings\matlab\Debug\OutputSBML.mexw64")
