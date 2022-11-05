76754accb923b74131e28b1bacdec85e7ffe0c4c
Sarah <s.keating@ucl.ac.uk>
2022-11-05 13:39:59 +0000

    add test files for fbc v3

---
+++ src/bindings/matlab/test/testReadFromFileFbc4.m
@@ -0,0 +1,148 @@
+function y = testReadFromFileFbc4(silent)
+
+filename = fullfile(pwd,'test-data', 'fbcV3_1.xml');
+
+m = TranslateSBML(filename);
+
+test = 48;
+Totalfail = 0;
+
+Totalfail = Totalfail + fail_unless(m.SBML_level == 3);
+Totalfail = Totalfail + fail_unless(m.SBML_version == 1);
+Totalfail = Totalfail + fail_unless(m.fbc_version == 3);
+Totalfail = Totalfail + fail_unless(m.fbc_strict == 1);
+
+
+%     <listOfSpecies>
+%      <species id="S" compartment="c" hasOnlySubstanceUnits="false" 
+%                        boundaryCondition="false" constant="false" 
+%                        fbc:charge="2" fbc:chemicalFormula="s20"/>
+%       <species id="S1" compartment="c" hasOnlySubstanceUnits="false" 
+%                        boundaryCondition="false" constant="false" 
+%                        fbc:charge="2" fbc:chemicalFormula="s20"/>
+%       <species id="S2" compartment="c" hasOnlySubstanceUnits="false" 
+%                        boundaryCondition="false" constant="false"/>
+%       <species id="S3" compartment="c" hasOnlySubstanceUnits="false" 
+%                        boundaryCondition="false" constant="false" 
+%                        fbc:charge="2" fbc:chemicalFormula="s20"/>
+%       <species id="S4" compartment="c" hasOnlySubstanceUnits="false" 
+%                        boundaryCondition="false" constant="false" 
+%                        fbc:charge="2" fbc:chemicalFormula="s20"/>
+%     </listOfSpecies>
+
+  Totalfail = Totalfail + fail_unless( length(m.species) == 5);
+
+  Totalfail = Totalfail + fail_unless( strcmp( m.species(1).id, 'S'             ));
+  Totalfail = Totalfail + fail_unless( strcmp( m.species(1).compartment, 'comp1' ));
+  Totalfail = Totalfail + fail_unless( m.species(1).isSetInitialAmount == 1);
+  Totalfail = Totalfail + fail_unless( m.species(1).isSetInitialConcentration == 0);
+  Totalfail = Totalfail + fail_unless( m.species(1).fbc_charge == 2);
+  Totalfail = Totalfail + fail_unless( m.species(1).isSetfbc_charge == 1);
+  Totalfail = Totalfail + fail_unless( strcmp( m.species(1).fbc_chemicalFormula, 'S20'             ));
+
+  Totalfail = Totalfail + fail_unless( strcmp( m.species(2).id, 'S1'             ));
+  Totalfail = Totalfail + fail_unless( strcmp( m.species(2).compartment, 'comp1' ));
+  Totalfail = Totalfail + fail_unless( m.species(2).isSetInitialAmount == 1);
+  Totalfail = Totalfail + fail_unless( m.species(2).isSetInitialConcentration == 0);
+  Totalfail = Totalfail + fail_unless( m.species(2).fbc_charge == 2);
+  Totalfail = Totalfail + fail_unless( m.species(2).isSetfbc_charge == 1);
+  Totalfail = Totalfail + fail_unless( strcmp( m.species(2).fbc_chemicalFormula, 'S20'             ));
+
+  Totalfail = Totalfail + fail_unless( strcmp( m.species(3).id, 'S2'             ));
+  Totalfail = Totalfail + fail_unless( strcmp( m.species(3).compartment, 'comp1' ));
+  Totalfail = Totalfail + fail_unless( m.species(3).isSetInitialAmount == 1);
+  Totalfail = Totalfail + fail_unless( m.species(3).isSetInitialConcentration == 0);
+  Totalfail = Totalfail + fail_unless( m.species(3).isSetfbc_charge == 0);
+  Totalfail = Totalfail + fail_unless( ~isempty( m.species(2).fbc_chemicalFormula));
+
+  Totalfail = Totalfail + fail_unless( m.species(3).level == 3);
+  Totalfail = Totalfail + fail_unless( m.species(3).version == 1);
+  Totalfail = Totalfail + fail_unless( m.species(3).fbc_version == 3);
+  
+
+ 
+  %    <fbc:listOfGeneProducts>
+  %     <fbc:geneProduct fbc:id="g_1" fbc:label="b1"/>
+  %   </fbc:listOfGeneProducts>
+
+  Totalfail = Totalfail + fail_unless( length(m.fbc_geneProduct) == 2);
+
+  Totalfail = Totalfail + fail_unless( strcmp(m.fbc_geneProduct(1).typecode, 'SBML_FBC_GENE_PRODUCT'));
+  Totalfail = Totalfail + fail_unless( strcmp(m.fbc_geneProduct(1).fbc_id, 'g_1'));
+  Totalfail = Totalfail + fail_unless( strcmp(m.fbc_geneProduct(1).fbc_label, 'g_1'));
+
+%         <fbc:geneProductAssociation fbc:id="gg1">
+%             <fbc:geneProductRef fbc:geneProduct="g_1"/>
+%          </fbc:geneProductAssociation>
+
+  gpa = m.reaction(1).fbc_geneProductAssociation;
+
+  Totalfail = Totalfail + fail_unless( strcmp(gpa.typecode, 'SBML_FBC_GENE_PRODUCT_ASSOCIATION'));
+  Totalfail = Totalfail + fail_unless( strcmp(gpa.fbc_id, 'gg1'));
+
+  Totalfail = Totalfail + fail_unless( length(gpa.fbc_association) == 1);
+
+  Totalfail = Totalfail + fail_unless( strcmp(gpa.fbc_association.typecode, 'SBML_FBC_GENE_PRODUCT_REF'));
+  Totalfail = Totalfail + fail_unless( strcmp(gpa.fbc_association.fbc_association, 'g_1'));
+  
+%        <fbc:geneProductAssociation fbc:id="gg1">
+%           <fbc:or metaid="ss">
+%             <fbc:geneProductRef fbc:geneProduct="g_1"/>
+%             <fbc:geneProductRef fbc:geneProduct="g_2"/>
+%           </fbc:or>
+%         </fbc:geneProductAssociation>
+  
+  gpa = m.reaction(2).fbc_geneProductAssociation;
+
+  Totalfail = Totalfail + fail_unless( strcmp(gpa.typecode, 'SBML_FBC_GENE_PRODUCT_ASSOCIATION'));
+  Totalfail = Totalfail + fail_unless( strcmp(gpa.fbc_id, 'gg2'));
+
+  Totalfail = Totalfail + fail_unless( length(gpa.fbc_association) == 1);
+
+  Totalfail = Totalfail + fail_unless( strcmp(gpa.fbc_association.typecode, 'SBML_FBC_OR'));
+  Totalfail = Totalfail + fail_unless( strcmp(gpa.fbc_association.fbc_association, '(g_1 or g_2)'));
+  
+%         <fbc:geneProductAssociation fbc:id="gg3">
+%           <fbc:or metaid="ss">
+%             <fbc:geneProductRef fbc:geneProduct="g_1"/>
+%             <fbc:geneProductRef fbc:geneProduct="g_2"/>
+%             <fbc:and>
+%               <fbc:geneProductRef fbc:geneProduct="g_1"/>
+%               <fbc:geneProductRef fbc:geneProduct="g_2"/>
+%             </fbc:and>
+%           </fbc:or>
+%         </fbc:geneProductAssociation>
+
+  gpa = m.reaction(3).fbc_geneProductAssociation;
+
+  Totalfail = Totalfail + fail_unless( strcmp(gpa.typecode, 'SBML_FBC_GENE_PRODUCT_ASSOCIATION'));
+  Totalfail = Totalfail + fail_unless( strcmp(gpa.fbc_id, 'gg3'));
+
+  Totalfail = Totalfail + fail_unless( length(gpa.fbc_association) == 1);
+
+  Totalfail = Totalfail + fail_unless( strcmp(gpa.fbc_association.typecode, 'SBML_FBC_OR'));
+  Totalfail = Totalfail + fail_unless( strcmp(gpa.fbc_association.fbc_association, '(g_1 or g_2 or (g_1 and g_2))'));
+  
+
+  
+if (silent == 0)
+disp('Testing readFromFileFbc4:');
+disp(sprintf('Number tests: %d', test));
+disp(sprintf('Number fails: %d', Totalfail));
+disp(sprintf('Pass rate: %d%%\n', ((test-Totalfail)/test)*100));
+end;
+
+if (Totalfail == 0)
+    y = 0;
+else
+    y = 1;
+end;
+
+function y = fail_unless(arg)
+
+if (~arg)
+    y = 1;
+else
+    y = 0;
+end;
+    
