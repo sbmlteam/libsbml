1770a5ec88fd1d1b16e79672b2c4a7db6c4c1f30
Sarah <s.keating@ucl.ac.uk>
2023-03-24 09:00:25 +0000

    start on xml node from file

--- src/sbml/xml/test/TestRunner.c
+++ src/sbml/xml/test/TestRunner.c
@@ -66,26 +66,64 @@ Suite *create_suite_XMLOutputStream (void);
 Suite *create_suite_XMLAttributes_C (void);
 Suite *create_suite_XMLExceptions (void);
 
+/**
+ * Global.
+ *
+ * Declared extern in TestAnnotation suite.
+ */
+char *TestDataDirectory;
+
+
+/**
+ * Sets TestDataDirectory for the the TestAnnotation suite.
+ *
+ * For Automake's distcheck target to work properly, TestDataDirectory must
+ * begin with the value of the environment variable SRCDIR.
+ */
+void
+setTestDataDirectory (void)
+{
+  char *srcdir = getenv("srcdir");
+  size_t  length  = (srcdir == NULL) ? 0 : strlen(srcdir);
+
+
+  /**
+   * strlen("/test-data/") = 11 + 1 (for NULL) = 12
+   */
+  TestDataDirectory = (char *) safe_calloc( length + 12, sizeof(char) );
+
+  if (srcdir != NULL)
+  {
+    strcpy(TestDataDirectory, srcdir);
+    strcat(TestDataDirectory, "/");
+  }
+
+  strcat(TestDataDirectory, "test-data/");
+}
+
+
 int
 main (int argc, char* argv[]) 
 { 
   int num_failed = 0;
-  SRunner *runner = srunner_create(create_suite_XMLAttributes());
-  srunner_add_suite(runner, create_suite_CopyAndClone());
+  setTestDataDirectory();
+  SRunner *runner = srunner_create(create_suite_XMLNode());
+  //SRunner *runner = srunner_create(create_suite_XMLAttributes());
+  //srunner_add_suite(runner, create_suite_CopyAndClone());
 
-  srunner_add_suite(runner, create_suite_XMLNamespaces());
-  srunner_add_suite(runner, create_suite_XMLTriple());
-  srunner_add_suite(runner, create_suite_XMLToken());
-  srunner_add_suite(runner, create_suite_XMLToken_newSetters());
-  srunner_add_suite(runner, create_suite_XMLNode());
-  srunner_add_suite(runner, create_suite_XMLNode_newSetters());
-  srunner_add_suite(runner, create_suite_XMLError());
-  srunner_add_suite(runner, create_suite_XMLError_C());
-  srunner_add_suite(runner, create_suite_XMLErrorLog());
-  srunner_add_suite(runner, create_suite_XMLInputStream());
-  srunner_add_suite(runner, create_suite_XMLOutputStream());
-  srunner_add_suite(runner, create_suite_XMLAttributes_C());
-  srunner_add_suite(runner, create_suite_XMLExceptions());
+  //srunner_add_suite(runner, create_suite_XMLNamespaces());
+  //srunner_add_suite(runner, create_suite_XMLTriple());
+  //srunner_add_suite(runner, create_suite_XMLToken());
+  //srunner_add_suite(runner, create_suite_XMLToken_newSetters());
+  //srunner_add_suite(runner, create_suite_XMLNode());
+  //srunner_add_suite(runner, create_suite_XMLNode_newSetters());
+  //srunner_add_suite(runner, create_suite_XMLError());
+  //srunner_add_suite(runner, create_suite_XMLError_C());
+  //srunner_add_suite(runner, create_suite_XMLErrorLog());
+  //srunner_add_suite(runner, create_suite_XMLInputStream());
+  //srunner_add_suite(runner, create_suite_XMLOutputStream());
+  //srunner_add_suite(runner, create_suite_XMLAttributes_C());
+  //srunner_add_suite(runner, create_suite_XMLExceptions());
 
   if (argc > 1 && !strcmp(argv[1], "-nofork"))
   {
