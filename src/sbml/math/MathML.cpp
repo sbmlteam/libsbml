aa1ad60ed208c3243e6568b0a1c81c2dc24d4521
Sarah <s.keating@ucl.ac.uk>
2023-03-24 13:42:36 +0000

    sorting xmlns for math element

--- src/sbml/math/MathML.cpp
+++ src/sbml/math/MathML.cpp
@@ -2129,6 +2129,9 @@ readMathML (XMLInputStream& stream, std::string reqd_prefix, bool inRead)
       
     if (elem.isStart() && elem.isEnd()) return node;
 
+    /* record and explicitly decared namespaces*/
+    node->setDeclaredNamespaces(&(elem.getNamespaces()));
+
     /* check that math tag is followed by an appropriate
      * tag
      */
@@ -2209,26 +2212,35 @@ writeMathML (const ASTNode* node, XMLOutputStream& stream, SBMLNamespaces *sbmln
   static const string uri = "http://www.w3.org/1998/Math/MathML";
 
   stream.startElement("math");
-  stream.writeAttribute("xmlns", uri);
+ // stream.writeAttribute("xmlns", uri);
 
   if (node) 
   {
-  // FIX-ME need to know what level and version
-  if (node->hasUnits())
-  {
-    unsigned int level = SBML_DEFAULT_LEVEL;
-    unsigned int version = SBML_DEFAULT_VERSION;
-    if (sbmlns != NULL)
+    // write any namespaces that have been specifically declared on math element
+    XMLNamespaces* xmlns = node->getDeclaredNamespaces();
+    if (xmlns != NULL)
     {
-      level = sbmlns->getLevel();
-      version = sbmlns->getVersion();
+      for (int i = 0; i < xmlns->getNumNamespaces(); i++)
+      {
+        stream.writeAttribute("xmlns", xmlns->getPrefix(i), xmlns->getURI(i));
+
+      }
     }
+    if (node->hasUnits())
+    {
+      unsigned int level = SBML_DEFAULT_LEVEL;
+      unsigned int version = SBML_DEFAULT_VERSION;
+      if (sbmlns != NULL)
+      {
+        level = sbmlns->getLevel();
+        version = sbmlns->getVersion();
+      }
     
-    stream.writeAttribute(XMLTriple("sbml", "", "xmlns"), 
-      SBMLNamespaces::getSBMLNamespaceURI(level, version));
-  }
+      stream.writeAttribute(XMLTriple("sbml", "", "xmlns"), 
+        SBMLNamespaces::getSBMLNamespaceURI(level, version));
+    }
   
-  writeNode(*node, stream,sbmlns);
+    writeNode(*node, stream,sbmlns);
   }
 
   stream.endElement("math");
