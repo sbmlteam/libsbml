cc1008ec2fdff1e3732bb5f1446365592f0ea5f1
Frank T. Bergmann <frank.thomas.bergmann@gmail.com>
2025-04-10 10:41:46 +0200

    - unify tabs and spaces

--- src/sbml/extension/test/TestSBMLDocumentPlugin.cpp
+++ src/sbml/extension/test/TestSBMLDocumentPlugin.cpp
@@ -69,35 +69,35 @@ extern char *TestDataDirectory;
 
 START_TEST (test_SBMLDocumentPlugin_create)
 {
-	TestPkgNamespaces ns(3, 1, 1);
-	string uri = TestExtension::getXmlnsL3V1V1();
-	string prefix = "prefix";
-	std::vector<std::string> packageURIs;
-	packageURIs.push_back(uri);
+  TestPkgNamespaces ns(3, 1, 1);
+  string uri = TestExtension::getXmlnsL3V1V1();
+  string prefix = "prefix";
+  std::vector<std::string> packageURIs;
+  packageURIs.push_back(uri);
 
-	// create a creator for TestModelPlugins	
-	SBaseExtensionPoint sbmldocExtPoint("core",SBML_DOCUMENT);
-	SBasePluginCreator<SBMLDocumentPlugin, TestExtension> sbmldocPluginCreator(sbmldocExtPoint,packageURIs);
+  // create a creator for TestModelPlugins  
+  SBaseExtensionPoint sbmldocExtPoint("core",SBML_DOCUMENT);
+  SBasePluginCreator<SBMLDocumentPlugin, TestExtension> sbmldocPluginCreator(sbmldocExtPoint,packageURIs);
 
-	fail_unless(sbmldocPluginCreator.getNumOfSupportedPackageURI() == 1);		
-	fail_unless(strcmp(sbmldocPluginCreator.getSupportedPackageURI(0).c_str(), uri.c_str()) == 0);
-	fail_unless(strcmp(sbmldocPluginCreator.getSupportedPackageURI(10000).c_str(), "") == 0);
-	fail_unless(sbmldocPluginCreator.getTargetExtensionPoint().getPackageName() == sbmldocExtPoint.getPackageName());
-	fail_unless(sbmldocPluginCreator.getTargetExtensionPoint().getTypeCode() == sbmldocExtPoint.getTypeCode());
-	fail_unless(sbmldocPluginCreator.getTargetPackageName() == sbmldocExtPoint.getPackageName());	
-	fail_unless(sbmldocPluginCreator.getTargetSBMLTypeCode() == sbmldocExtPoint.getTypeCode());		
-	fail_unless(sbmldocPluginCreator.isSupported(uri));	
+  fail_unless(sbmldocPluginCreator.getNumOfSupportedPackageURI() == 1);    
+  fail_unless(strcmp(sbmldocPluginCreator.getSupportedPackageURI(0).c_str(), uri.c_str()) == 0);
+  fail_unless(strcmp(sbmldocPluginCreator.getSupportedPackageURI(10000).c_str(), "") == 0);
+  fail_unless(sbmldocPluginCreator.getTargetExtensionPoint().getPackageName() == sbmldocExtPoint.getPackageName());
+  fail_unless(sbmldocPluginCreator.getTargetExtensionPoint().getTypeCode() == sbmldocExtPoint.getTypeCode());
+  fail_unless(sbmldocPluginCreator.getTargetPackageName() == sbmldocExtPoint.getPackageName());  
+  fail_unless(sbmldocPluginCreator.getTargetSBMLTypeCode() == sbmldocExtPoint.getTypeCode());    
+  fail_unless(sbmldocPluginCreator.isSupported(uri));  
 
-	SBMLDocumentPlugin *plugin = sbmldocPluginCreator.createPlugin(uri, prefix, ns.getNamespaces());
+  SBMLDocumentPlugin *plugin = sbmldocPluginCreator.createPlugin(uri, prefix, ns.getNamespaces());
 
-	fail_unless(plugin != NULL);
+  fail_unless(plugin != NULL);
   fail_unless(plugin->isSetRequired() == false);
-	fail_unless(plugin->getRequired() == true);
-	plugin->setRequired(false);
+  fail_unless(plugin->getRequired() == true);
+  plugin->setRequired(false);
   fail_unless(plugin->isSetRequired() == true);
-	fail_unless(plugin->getRequired() == false);
+  fail_unless(plugin->getRequired() == false);
 
-	delete plugin;
+  delete plugin;
 
 
 }
@@ -106,26 +106,26 @@ END_TEST
 START_TEST (test_SBMLDocumentPlugin_c_api)
 {
   TestPkgNamespaces ns(3, 1, 1);
-	string uri = TestExtension::getXmlnsL3V1V1();
-	string prefix = "prefix";
-	std::vector<std::string> packageURIs;
-	packageURIs.push_back(uri);
+  string uri = TestExtension::getXmlnsL3V1V1();
+  string prefix = "prefix";
+  std::vector<std::string> packageURIs;
+  packageURIs.push_back(uri);
 
-	// create a creator for TestModelPlugins	
-	SBaseExtensionPoint sbmldocExtPoint("core",SBML_DOCUMENT);
-	SBasePluginCreator<SBMLDocumentPlugin, TestExtension> sbmldocPluginCreator(sbmldocExtPoint,packageURIs);
+  // create a creator for TestModelPlugins  
+  SBaseExtensionPoint sbmldocExtPoint("core",SBML_DOCUMENT);
+  SBasePluginCreator<SBMLDocumentPlugin, TestExtension> sbmldocPluginCreator(sbmldocExtPoint,packageURIs);
 
-	fail_unless(sbmldocPluginCreator.getNumOfSupportedPackageURI() == 1);		
-	fail_unless(strcmp(sbmldocPluginCreator.getSupportedPackageURI(0).c_str(), uri.c_str()) == 0);
-	fail_unless(strcmp(sbmldocPluginCreator.getSupportedPackageURI(10000).c_str(), "") == 0);
-	fail_unless(sbmldocPluginCreator.getTargetExtensionPoint().getPackageName() == sbmldocExtPoint.getPackageName());
-	fail_unless(sbmldocPluginCreator.getTargetExtensionPoint().getTypeCode() == sbmldocExtPoint.getTypeCode());
-	fail_unless(sbmldocPluginCreator.getTargetPackageName() == sbmldocExtPoint.getPackageName());	
-	fail_unless(sbmldocPluginCreator.getTargetSBMLTypeCode() == sbmldocExtPoint.getTypeCode());		
-	fail_unless(sbmldocPluginCreator.isSupported(uri));	
+  fail_unless(sbmldocPluginCreator.getNumOfSupportedPackageURI() == 1);    
+  fail_unless(strcmp(sbmldocPluginCreator.getSupportedPackageURI(0).c_str(), uri.c_str()) == 0);
+  fail_unless(strcmp(sbmldocPluginCreator.getSupportedPackageURI(10000).c_str(), "") == 0);
+  fail_unless(sbmldocPluginCreator.getTargetExtensionPoint().getPackageName() == sbmldocExtPoint.getPackageName());
+  fail_unless(sbmldocPluginCreator.getTargetExtensionPoint().getTypeCode() == sbmldocExtPoint.getTypeCode());
+  fail_unless(sbmldocPluginCreator.getTargetPackageName() == sbmldocExtPoint.getPackageName());  
+  fail_unless(sbmldocPluginCreator.getTargetSBMLTypeCode() == sbmldocExtPoint.getTypeCode());    
+  fail_unless(sbmldocPluginCreator.isSupported(uri));  
 
   SBMLDocumentPlugin_t* plugin = SBMLDocumentPlugin_create(uri.c_str(), prefix.c_str(), &ns);
-	fail_unless(plugin != NULL);
+  fail_unless(plugin != NULL);
   fail_unless(SBMLDocumentPlugin_isSetRequired(plugin) == (int)false);
   fail_unless(SBMLDocumentPlugin_getRequired(plugin) == (int)true);
   SBMLDocumentPlugin_setRequired(plugin, (int)true); 
@@ -164,19 +164,19 @@ START_TEST(test_SBMLDocumentPlugin_read)
 
   if (layout_enabled) {
     // layout is enabled, so we should not have the l2 required attribute 
-	// on the document
+  // on the document
     fail_unless(sbml.find("layout_L2:") == std::string::npos);
   }
   else {
     // layout is not enabled, so we should have the l2 required attribute 
-	// on the document
+  // on the document
     fail_unless(sbml.find("layout_L2:") != std::string::npos);
   }
 
   SBMLDocument* roundtrip = readSBMLFromString(sbml.c_str());
   fail_unless(roundtrip->getNumErrors(LIBSBML_SEV_ERROR) == 0);
 
-	std::string sbml2 = writeSBMLToString(roundtrip);
+  std::string sbml2 = writeSBMLToString(roundtrip);
 
 
   delete roundtrip;
@@ -189,7 +189,7 @@ create_suite_SBMLDocumentPlugin (void)
 {
   Suite *suite = suite_create("SBMLDocumentPlugin");
   TCase *tcase = tcase_create("SBMLDocumentPlugin");
-	
+  
   tcase_add_test( tcase, test_SBMLDocumentPlugin_create );
   tcase_add_test( tcase, test_SBMLDocumentPlugin_c_api );
   tcase_add_test( tcase, test_SBMLDocumentPlugin_read );
