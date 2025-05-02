31a6ce30cd484fbb8470cb9779c5092a92aa0ddb
Lucian Smith <lpsmith@uw.edu>
2025-05-02 15:50:27 -0700

    Encode all &, <, and > in markdown.
    This is non-standard markdown!  But it has the advantage of creating valid HTML more often, so I think on balance it's a win?
    
    This means that this translator will *not* translate valid HTML in markdown to actual HTML.  I figured this was a more uncommon occurrence than (say) all the biomodels notes that have "<=>" as a reaction symbol, or that have '&' in a URL.
    
    We can drop this (I think) if https://github.com/tim-gromeyer/html2md/issues/158 is addressed.  But Maddy might need updating as well.
    
    Anyway: hack for now, but happy to revert if people think the cost is too severe.

--- src/sbml/util/util.cpp
+++ src/sbml/util/util.cpp
@@ -558,7 +558,22 @@ std::string util_markdown_to_html(const std::string& markdown)
     //config->enabledParsers |= maddy::types::HTML_PARSER; // do not wrap HTML in paragraph
     //std::shared_ptr<maddy::Parser> parser = std::make_shared<maddy::Parser>(config);
 
-    std::stringstream markdownInput(markdown);
+    //Note:  tried to figure out difference between genuine HTML-ish of &, < and > vs. 
+    // ones that needed to be encoded, but failed. Everything will officially
+    // translate if we encode them all indiscriminately, so hey!  Here we go.
+    std::regex pattern("&");
+    std::string copy = std::regex_replace(markdown, pattern, "&amp;");
+
+    pattern = "&amp;amp;";
+    copy = std::regex_replace(copy, pattern, "&amp;");
+
+    pattern = "<";
+    copy = std::regex_replace(copy, pattern, "&lt;");
+
+    pattern = ">";
+    copy = std::regex_replace(copy, pattern, "&gt;");
+
+    std::stringstream markdownInput(copy);
     static maddy::Parser parser;
     return parser.Parse(markdownInput);
 }
