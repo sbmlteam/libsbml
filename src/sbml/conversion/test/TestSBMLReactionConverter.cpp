a89f82202ca562c5c0338a1922f2499098fae4d9
Sarah Keating <s.keating@ucl.ac.uk>
2025-05-26 12:59:10 +0100

    Disables individual test case execution
    for debugging or
    temporary exclusion purposes.

--- src/sbml/conversion/test/TestSBMLReactionConverter.cpp
+++ src/sbml/conversion/test/TestSBMLReactionConverter.cpp
@@ -322,13 +322,13 @@ END_TEST
 Suite*
 create_suite_TestSBMLReactionConverter(void)
 {
-	bool testing = true;
+	bool testing_individual = false;
 	Suite* suite = suite_create("SBMLReactionConverter");
 	TCase* tcase = tcase_create("SBMLReactionConverter");
 	tcase_add_checked_fixture(tcase, Reaction_setup,
 		Reaction_teardown);
 
-	if (testing)
+	if (testing_individual)
 	{
 		tcase_add_test(tcase, test_reactionconversion_012);
 	}
