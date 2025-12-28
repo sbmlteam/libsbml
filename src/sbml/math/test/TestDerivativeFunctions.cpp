61a1f6fe60e35cae3f7b0c555d9df5f3e5accbb7
Sarah Keating <s.keating@ucl.ac.uk>
2025-12-28 12:02:45 +0000

    add header

--- src/sbml/math/test/TestDerivativeFunctions.cpp
+++ src/sbml/math/test/TestDerivativeFunctions.cpp
@@ -48,6 +48,7 @@
 
 #include <sbml/math/FormulaParser.h>
 #include <sbml/math/FormulaFormatter.h>
+#include <sbml/math/L3FormulaFormatter.h>
 #include <sbml/math/ASTNode.h>
 #include <sbml/math/MathML.h>
 
