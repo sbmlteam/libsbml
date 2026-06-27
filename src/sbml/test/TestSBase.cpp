bc0e4c41c7b445655d18f9933d0ec876082fb926
dyrpsf <dyrpsf@gmail.com>
2026-06-27 16:05:55 +0530

    Add unit test for unsetModifiedDates (Issue #354)

--- src/sbml/test/TestSBase.cpp
+++ src/sbml/test/TestSBase.cpp
@@ -2654,6 +2654,35 @@ START_TEST(test_SBase_userData1)
 }
 END_TEST
 
+START_TEST (test_SBase_unsetModifiedDates_issue354)
+{
+  Model* m = new Model(3, 2);
+  m->setMetaId("foo");
+  
+  ModelHistory* h = new ModelHistory();
+  Date* d = new Date("2019-07-29T10:53:09Z");
+  h->addModifiedDate(d);
+  
+  m->setModelHistory(h);
+  
+  // Ensure the date was successfully added
+  fail_unless(m->getNumModifiedDates() == 1);
+  
+  // Unset the dates (This triggers the new cleanup logic we wrote in SBase.cpp)
+  m->unsetModifiedDates();
+  
+  // Force the annotation to sync and generate the XML string
+  std::string annotation = m->getAnnotationString();
+  
+  // Assert that the empty Description detritus was completely removed
+  size_t found = annotation.find("<rdf:Description rdf:about=\"#foo\"/>");
+  fail_unless(found == std::string::npos);
+  
+  delete d;
+  delete m;
+}
+END_TEST
+
 Suite *
 create_suite_SBase (void)
 {
@@ -2709,6 +2738,7 @@ create_suite_SBase (void)
   tcase_add_test(tcase, test_SBase_userData1);
 
   tcase_add_test(tcase, test_SBase_prefixMetaIdSBO);
+  tcase_add_test(tcase, test_SBase_unsetModifiedDates_issue354);
 
   suite_add_tcase(suite, tcase);
 
