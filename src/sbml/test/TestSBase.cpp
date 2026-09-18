2d1589739a8ae358c23b388a9ed19caebe07ff34
Lucian Smith <lpsmith@uw.edu>
2026-09-18 09:20:01 -0700

    Update maddy to simplify regexes, so VS2026 won't complain.
    A round-tripped notes from Biomodels 12 was causing maddy to run into throws from the regexes when compiled with VS2026 (not 2022, nor from any other compiler).  The new regexes are simpler, and should work for more complicated strings, like the one found in biomodels 12.

--- src/sbml/test/TestSBase.cpp
+++ src/sbml/test/TestSBase.cpp
@@ -705,6 +705,24 @@ START_TEST(test_SBase_notesMarkdown_table)
 END_TEST
 
 
+START_TEST(test_SBase_notesMarkdown_denseLinks)
+{
+    /* A single notes line carrying several Markdown links back to back,
+     * with no line break between them (from BIOMD0000000012 -- see
+     * antimony issue #177), drove std::regex past its complexity limit
+     * under MSVC's stricter <regex> implementation. */
+    Model* m = new(std::nothrow) Model(3, 1);
+    std::string notes = "( [KISAO_0000035](http://identifiers.org/biomodels.kisao/KISAO_0000035 \"Access to: KISAO_0000035\") ) with continuous variables ( [KISAO_0000018](http://identifiers.org/biomodels.kisao/KISAO_0000018 \"Access to: KISAO_0000018\") ). One sample algorithm to use is the CVODE solver ( [KISAO_0000019](http://identifiers.org/biomodels.kisao/KISAO_0000019 \"Access to: KISAO_0000019\") ). Second, one could simulate the system using Gillespie&apos;s direct method ( [KISAO_0000029](http://identifiers.org/biomodels.kisao/KISAO_0000029 \"Access to: KISAO_0000029\") ), which is a stochastic method ( [KISAO_0000036](http://identifiers.org/biomodels.kisao/KISAO_0000036 \"Access to: KISAO_0000036\") ) supporting adaptive timesteps ( [KISAO_0000041](http://identifiers.org/biomodels.kisao/KISAO_0000041 \"Access to: KISAO_0000041\") ) and using discrete variables ( [KISAO_0000016](http://identifiers.org/biomodels.kisao/KISAO_0000016 \"Access to: KISAO_0000016\") ). ";
+
+    fail_unless(m->setNotesFromMarkdown(notes) == LIBSBML_OPERATION_SUCCESS);
+
+    std::string html = m->getNotesString();
+    fail_unless(html.find("<a href=\"http://identifiers.org/biomodels.kisao/KISAO_0000035\" title=\"Access to: KISAO_0000035\">KISAO_0000035</a>") != std::string::npos);
+    fail_unless(html.find("<a href=\"http://identifiers.org/biomodels.kisao/KISAO_0000016\" title=\"Access to: KISAO_0000016\">KISAO_0000016</a>") != std::string::npos);
+}
+END_TEST
+
+
 START_TEST(test_SBase_setAnnotationString)
 {
   const char * annotation = "This is a test note";
@@ -2729,6 +2747,7 @@ create_suite_SBase (void)
   tcase_add_test(tcase, test_SBase_setNotesFromMarkdown2);
   tcase_add_test(tcase, test_SBase_setNotesFromMarkdown3);
   tcase_add_test(tcase, test_SBase_notesMarkdown_table);
+  tcase_add_test(tcase, test_SBase_notesMarkdown_denseLinks);
   tcase_add_test(tcase, test_SBase_setAnnotationString);
   tcase_add_test(tcase, test_SBase_unsetAnnotationWithCVTerms );
   tcase_add_test(tcase, test_SBase_unsetAnnotationWithModelHistory );
